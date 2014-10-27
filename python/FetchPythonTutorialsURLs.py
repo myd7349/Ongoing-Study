fdba81095ec81fd1e9f1df6658f7640f8600feaf
myd7349 <myd7349@gmail.com>
2014-10-27 15:10:16 +0800

    Refactor  FetchPythonTutorialsURLs.py
    Make it more efficient

--- python/FetchPythonTutorialsURLs.py
+++ python/FetchPythonTutorialsURLs.py
@@ -3,41 +3,55 @@
 
 # 2014-10-27 10:43 (UTC+8)
 
-def main():
-    import os
-    import re
+def fetchPage(url, encoding = 'utf-8'):
+    '''Fetch the source code of specified web page line by line.'''
     import urllib.request
 
-    response = urllib.request.urlopen('http://www.bogotobogo.com/python/pytut.php')
-    page_encoding = response.headers.get_content_charset(failobj = 'utf-8')
-    page_contents = response.read().decode(page_encoding)
+    response = urllib.request.urlopen(url)
+    charset = response.headers.get_content_charset(failobj = encoding)
 
-    target_file = os.path.join(os.environ['HOME'], 'page.txt')
-    with open(target_file, 'w') as fp:
-        fp.write(page_contents)
+    if encoding:
+        for line in response:
+            yield line.decode(charset)
+    else:
+        for line in response:
+            yield line
 
-    python_tutorial_urls = []
-    url_pattern = re.compile(r'<a href="(.*)">')
-    with open(target_file) as fp:
-        # skip several lines until we reach the start line of <List of Python Tutorials>
-        for line in iter(fp.readline, '<h1>List of Python Tutorials</h1>\n'):
-            pass
-        # 
-        for line in iter(fp.readline, '<ul>\n'):
-            pass
-        #
-        for line in iter(fp.readline, '</ul>\n'):
+def main():
+    '''Extract all Python tutorials urls from this page:
+    http://www.bogotobogo.com/python/pytut.php
+    '''
+    import itertools
+    import os
+    import re
+
+    line_generator = fetchPage('http://www.bogotobogo.com/python/pytut.php')
+
+    # We can call builtin function iter in two ways(Please refer to the manual):
+    # 1. iter(iterable)
+    # 2. iter(callable, sentinel)
+    # It seems that we can't pass a generator function to 'callable' in the 2nd
+    # form, so I use itertools.takewhile here.
+    for line in itertools.takewhile(
+        lambda x: x.strip() != '<h1>List of Python Tutorials</h1>',
+        line_generator):
+        pass
+
+    # Try to find the HTML list's start tag: <ul>
+    for line in itertools.takewhile(lambda x: x.strip() != '<ul>',
+                                    line_generator):
+        pass
+
+    # Extract all the urls in the HTML list until we reach the end tag: </ul>
+    with open(os.path.join(os.environ['HOME'], 'List_of_Python_Tutorials.txt'),
+              'w') as fp:
+        url_pattern = re.compile(r'<a href="(.*)">')
+        
+        for line in itertools.takewhile(lambda x: x.strip() != '</ul>',
+                                        line_generator):
             search_res = url_pattern.search(line)
             if search_res:
-                python_tutorial_urls.append(search_res.group(1))
-    #
-    os.remove(target_file)
-
-    # Now, we got all the urls of <List of Python Tutorials>, process this list
-    with open(os.path.join(os.environ['HOME'], 'List_of_Python_Tutorials.txt'), 'w') as fp:
-        for url in python_tutorial_urls:
-            print(url, file = fp)
+                print(search_res.group(1), file = fp)
 
 if __name__ == '__main__':
     main()
-    
