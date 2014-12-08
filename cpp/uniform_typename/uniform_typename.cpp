6572443891dbfcb30384aba3b1651ee1e15e4151
myd7349 <myd7349@gmail.com>
2014-12-08 11:00:19 +0800

    Move the TYPE_NAME defination into a header file

--- cpp/uniform_typename/uniform_typename.cpp
+++ cpp/uniform_typename/uniform_typename.cpp
@@ -8,40 +8,7 @@
 #include <iostream>
 #include <typeinfo>
 
-// What predefined macro can I use to detect clang/gcc/msvc?
-// http://nadeausoftware.com/articles/2012/10/c_c_tip_how_detect_compiler_name_and_version_using_compiler_predefined_macros
-// http://stackoverflow.com/questions/2658461/what-predefined-macro-can-i-use-to-detect-clang
-#if defined(__clang__)
-//# define TYPE_NAME(exp) (typeid(exp).name())
-# error Not tested yet!
-#elif defined(__GNUC__) || defined(__GNUG__)
-// The type name we got from g++ is a decorated name, so we need demangle it.
-// http://stackoverflow.com/questions/4465872/why-typeid-name-returns-weird-characters-using-gcc
-// https://gcc.gnu.org/onlinedocs/libstdc++/manual/ext_demangling.html
-# include <cassert>
-# include <string>
-# include <cxxabi.h>
-
-std::string demangle(const char *mangledName)
-{
-    assert(mangledName != nullptr);
-
-    int status;
-    char *demangledName = abi::__cxa_demangle(mangledName, nullptr, nullptr, &status);
-
-    if (demangledName != nullptr) {
-        std::string realName = demangledName;
-        delete demangledName;
-        return realName;
-    } else {
-        return mangledName;
-    }
-}
-
-# define TYPE_NAME(exp) (demangle(typeid(exp).name()))
-#elif defined(_MSC_VER)
-# define TYPE_NAME(exp) (typeid(exp).name())
-#endif
+#include "uniform_typename.h"
 
 int main()
 {
---
+++ cpp/uniform_typename/uniform_typename.h
@@ -0,0 +1,40 @@
+// 2014-12-08T10:56+08:00
+#ifndef UNIFORM_TYPENAME_H_INCLUDED
+#define UNIFORM_TYPENAME_H_INCLUDED
+
+// What predefined macro can I use to detect clang/gcc/msvc?
+// http://nadeausoftware.com/articles/2012/10/c_c_tip_how_detect_compiler_name_and_version_using_compiler_predefined_macros
+// http://stackoverflow.com/questions/2658461/what-predefined-macro-can-i-use-to-detect-clang
+#if defined(__clang__)
+//# define TYPE_NAME(exp) (typeid(exp).name())
+# error Not tested yet!
+#elif defined(__GNUC__) || defined(__GNUG__)
+// The type name we got from g++ is a decorated name, so we need demangle it.
+// http://stackoverflow.com/questions/4465872/why-typeid-name-returns-weird-characters-using-gcc
+// https://gcc.gnu.org/onlinedocs/libstdc++/manual/ext_demangling.html
+# include <cassert>
+# include <string>
+# include <cxxabi.h>
+
+std::string demangle(const char *mangledName)
+{
+    assert(mangledName != nullptr);
+
+    int status;
+    char *demangledName = abi::__cxa_demangle(mangledName, nullptr, nullptr, &status);
+
+    if (demangledName != nullptr) {
+        std::string realName = demangledName;
+        delete demangledName;
+        return realName;
+    } else {
+        return mangledName;
+    }
+}
+
+# define TYPE_NAME(exp) (demangle(typeid(exp).name()))
+#elif defined(_MSC_VER)
+# define TYPE_NAME(exp) (typeid(exp).name())
+#endif
+
+#endif // UNIFORM_TYPENAME_H_INCLUDED
