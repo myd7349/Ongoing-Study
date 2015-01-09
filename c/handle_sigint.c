cd65ee7b7aa4d3c2ee32c2138ebcf4755987087f
myd7349 <myd7349@126.com>
2015-01-09 16:52:35 +0800

    Add an example for signal handling in C

---
+++ c/handle_sigint.c
@@ -0,0 +1,35 @@
+/* 2015-01-09T16:30+08:00  */
+
+/* Signal handling and non-local jump test on CentOS. */
+
+#include <setjmp.h>
+#include <signal.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef void (*sig_handler)(int);
+
+jmp_buf env_buf;
+
+void sigint_handler(int parm)
+{
+    siglongjmp(env_buf, 1);
+}
+
+int main(void)
+{
+    sig_handler prev_handler = signal(SIGINT, sigint_handler);
+    if (SIG_ERR == prev_handler) {
+        fputs("Failed to register SIGINT handler\n", stderr);
+        return EXIT_FAILURE;
+    }
+
+    if (!sigsetjmp(env_buf, SIGINT)) {
+        puts("We have a lot of work to do in our whole life...");
+        while (1);
+    } else {
+        puts("Now, just take a rest and enjoy this journey!");
+    }
+
+    return EXIT_SUCCESS;    
+}
