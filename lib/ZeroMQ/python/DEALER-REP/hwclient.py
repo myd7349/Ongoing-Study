881f1d41af50111f48d020e725d217717ff83feb
myd7349 <myd7349@126.com>
2015-01-15 17:39:41 +0800

    Update hwclient.py

--- lib/ZeroMQ/python/DEALER-REP/hwclient.py
+++ lib/ZeroMQ/python/DEALER-REP/hwclient.py
@@ -33,16 +33,19 @@ def main():
     # that can talk to multiple REP servers. And we'd be able to send off any
     # number of "Hello" requests without waiting for replies.
     for i in range(request_cnt):
-        # Send an empty message part as the delimiter...
+        # Step 1: Send an empty message part as the delimiter;
         socket.send(b'', flags = zmq.SNDMORE)
-        # then followed by one or more body parts.
+        # Step 2: then followed by one or more body parts.
         socket.send_string('Hello #{}'.format(i))
 
     for i in range(request_cnt):
+        # When we receive a message via a DEALER socket
+        # Step 1: Receive the first frame and if it's not empty, discard the
+        #         whole message;
         message = socket.recv()
+        # Step 2: Receive the next frame and pass that to the application.
         if not message:
-            message = socket.recv()
-        print('Received message:', message.decode('utf-8'))
+            print('Received message:', socket.recv_string())
 
 if __name__ == '__main__':    
     main()
