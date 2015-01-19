b018d3884e21e478c05af4778d15876aefc1c6a5
myd7349 <myd7349@gmail.com>
2015-01-19 15:03:23 +0800

    Add test for std::move working with a std::string

--- cpp/C++11/move_semantics_test/move_semantics_test.cpp
+++ cpp/C++11/move_semantics_test/move_semantics_test.cpp
@@ -1,6 +1,8 @@
 // 2015-01-19T14:10+08:00
 #include <cassert>
+#include <iostream>
 #include <memory>
+#include <string>
 #include <utility> // for std::move
 #include <vector>
 
@@ -17,7 +19,7 @@ void test_0()
     // operation, the value of the moved-from object should only be destroyed
     // or assigned a new value; accessing it otherwise yields an unspecified
     // value.
-    assert(arr.empty()); // ??? Is it safe?
+    assert(arr.empty()); // arr MIGHT be empty
 }
 
 void test_1()
@@ -30,15 +32,31 @@ void test_1()
     assert(nullptr == pi && pi2 != nullptr && 42 == *pi2);
 }
 
+// cppreference:
+// string move assignment operator is often implemented as swap,
+// in this case, the moved-from object is NOT empty
+void test_2()
+{
+    std::string str1 = "Hello";
+    std::string str2 = "World";
+
+    str2 = std::move(str1);
+
+    std::cout << str1 << ' ' << str2 << std::endl;
+}
+
 int main()
 {
     test_0();
 
     test_1();
 
+    test_2();
+
     return 0;
 }
 
 // References:
 // [std::move](http://www.cplusplus.com/reference/utility/move/)
+// [std::move](http://en.cppreference.com/w/cpp/utility/move)
 // [When should I use std::move in C++11?](http://www.quora.com/When-should-I-use-std-move-in-C++11)
