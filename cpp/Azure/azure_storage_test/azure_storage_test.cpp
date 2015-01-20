5213b6281b84f1819e93b020f3859afc230e2ea9
myd7349 <myd7349@126.com>
2015-01-20 16:55:37 +0800

    Update azure_storage_test.cpp

--- cpp/Azure/azure_storage_test/azure_storage_test.cpp
+++ cpp/Azure/azure_storage_test/azure_storage_test.cpp
@@ -4,17 +4,23 @@
 // Several official examples can be found here:
 // https://github.com/Azure/azure-storage-cpp/blob/master/Microsoft.WindowsAzure.Storage/samples/
 
+#include <chrono>
 #include <cstdlib>
 #include <exception>
 #include <iostream>
-#include <stdexcept>
+#include <locale>
 
+#include <boost/filesystem.hpp>
+#include <boost/progress.hpp>
 #include <boost/property_tree/ini_parser.hpp>
+//#include <boost/uuid/uuid.hpp>
 
+#include <cpprest/filestream.h>
+
+#include <wascore/basic_types.h>
 #include <was/blob.h>
 #include <was/common.h>
 #include <was/storage_account.h>
-#include <wascore/basic_types.h>
 
 #ifdef _MSC_VER
 # ifdef NDEBUG
@@ -26,42 +32,55 @@
 # pragma comment(lib, "wastorage.lib")
 #endif
 
-#if !defined(NDEBUG) && defined(_MSC_VER)
-# define PAUSE() (std::system("pause")) 
+#ifdef _MSC_VER
+# ifdef NDEBUG
+#  define PAUSE() (__noop)
+# else
+#  define PAUSE() std::system("pause")
+# endif
 #else
-# define PAUSE() (__noop)
+# define PAUSE() ((void)0)
 #endif
 
-#define RETURN_ON_FAILURE() std::cerr << e.what() << '\n'; PAUSE(); return EXIT_FAILURE
+#define RETURN_ON_FAILURE(msg) std::cerr << msg << ": " << e.what() << '\n'; PAUSE(); return EXIT_FAILURE
 
 namespace AS = azure::storage;
 namespace Cfg = boost::property_tree;
 
 int main(int argc, char *argv[])
 {
+    //std::locale::global(std::locale(""));
+
     // parse the configuration file and fetch out account name, account key, host endpoint
     Cfg::basic_ptree<utility::string_t, utility::string_t> cfg;
     try {
-        // The structure of "azure_storage_test.cfg" is described in this example:
-        // https://github.com/myd7349/Ongoing-Study/blob/master/python/Azure/azure_storage_test/azure_storage_test.py
+        // You can create your own configuration file based on "azure_storage_test.cfg.template".
         Cfg::read_ini("azure_storage_test.cfg", cfg);
     } catch (const Cfg::ini_parser_error &e) {
-        RETURN_ON_FAILURE();
+        RETURN_ON_FAILURE("Parsing configuration file failed:");
     }
 
     utility::string_t accountName;
-    utility::string_t accountKey;
+    utility::string_t primaryAccessKey;
+    utility::string_t secondaryAccessKey;
     // I am using Microsoft Azure China service, so my endpoint is: core.chinacloudapi.cn
-    utility::string_t endPoint = U("core.chinacloudapi.cn");
+    utility::string_t endPoint;
+    bool useHttps;
+    bool useDevAccount;
 
     try {
-        accountName = cfg.get<utility::string_t>(U("ACCOUNT.name"));
-        accountKey = cfg.get<utility::string_t>(U("ACCOUNT.key"));
-        //endPoint = cfg.get<utility::string_t>(U("ACCOUNT.host"));
+        accountName = cfg.get<utility::string_t>(U("ACCOUNT.account_name"));
+        primaryAccessKey = cfg.get<utility::string_t>(U("ACCOUNT.primary_access_key"));
+        secondaryAccessKey = cfg.get<utility::string_t>(U("ACCOUNT.secondary_access_key"));
+        endPoint = cfg.get<utility::string_t>(U("ACCOUNT.end_point"));
+        useHttps = cfg.get<bool>(U("ACCOUNT.use_https"));
+        useDevAccount = cfg.get<bool>(U("ACCOUNT.use_dev_account"));
     } catch (const Cfg::ptree_bad_path &e) {
-        RETURN_ON_FAILURE();
+        RETURN_ON_FAILURE("Specified key not found");
     } catch (const Cfg::ptree_bad_data &e) {
-        RETURN_ON_FAILURE();
+        RETURN_ON_FAILURE("Bad data");
+    } catch (const std::exception &e) {
+        RETURN_ON_FAILURE("Fetching configuration information failed");
     }
 
     AS::cloud_storage_account asAccount;
@@ -69,10 +88,17 @@ int main(int argc, char *argv[])
     // initialize the storage account
 #if 1
     try {
-        AS::storage_credentials credential(accountName, accountKey);
-        asAccount = AS::cloud_storage_account(credential, endPoint, true);
-    } catch (const std::runtime_error &e) {
-        RETURN_ON_FAILURE();
+        if (useDevAccount) {
+            // If you want to use a development storage account, please read this article: 
+            // Using the Azure Storage Emulator for Development and Testing
+            // http://msdn.microsoft.com/en-us/library/azure/hh403989.aspx
+            asAccount = AS::cloud_storage_account::development_storage_account();
+        } else {
+            AS::storage_credentials credential(accountName, primaryAccessKey);
+            asAccount = AS::cloud_storage_account(credential, endPoint, useHttps);
+        }
+    } catch (const std::exception &e) {
+        RETURN_ON_FAILURE("Creating storage account object failed");
     }
 #else
     // The workflow of parse:
@@ -80,32 +106,85 @@ int main(int argc, char *argv[])
     //   2. parse_defaults_settings
     //   3. parse_explicit_settings
     // In cloud_storage_account.cpp, we can see all the valid setting strings.
-    utility::string_t connStr = U("DefaultEndpointsProtocol=https;AccountName=")
-        + accountName + U(";AccountKey=") + accountKey + U(";EndpointSuffix=") + endPoint;
+    utility::string_t connStr;
+    if (useDevAccount) {
+        connStr = U("UseDevelopmentStorage=true;DevelopmentStorageProxyUri=;");
+    } else {
+        connStr =
+            utility::string_t(U("DefaultEndpointsProtocol=")) + (useHttps ? U("https;") : U("http;")) +
+            U("AccountName=") + accountName + U(";") +
+            U("AccountKey=") + primaryAccessKey + U(";") +
+            U("EndpointSuffix=") + endPoint + U(";");
+    }
 
     try {
         asAccount = AS::cloud_storage_account::parse(connStr);
-    } catch (const std::invalid_argument &e) {
-        RETURN_ON_FAILURE();
+    } catch (const std::exception &e) {
+        RETURN_ON_FAILURE("Parsing connection string failed");
     }
 #endif
 
-    AS::cloud_blob_client blobClient = asAccount.create_cloud_blob_client();
-    // Caution:
-    // A container name must be a valid DNS name. Please read the naming convention described here:
-    // http://msdn.microsoft.com/en-us/library/azure/dd135715.aspx
-    AS::cloud_blob_container container = blobClient.get_container_reference(U("myd7349"));
-    try {
-        container.create_if_not_exists();
-    } catch (const std::exception &e) {
-        RETURN_ON_FAILURE();
-    }
+    AS::blob_request_options reqOptions;
+    reqOptions.set_stream_read_size_in_bytes(1024 * 1024);
+    reqOptions.set_stream_write_size_in_bytes(1024 * 1024);
+    reqOptions.set_server_timeout(std::chrono::seconds(5));
+    reqOptions.set_store_blob_content_md5(true);
 
-    // set access permission
-    AS::blob_container_permissions permission;
-    permission.set_public_access(AS::blob_container_public_access_type::blob);
-    container.upload_permissions(permission);
+    try {
+        AS::cloud_blob_client blobClient = asAccount.create_cloud_blob_client(reqOptions);
+        // Caution:
+        // A container name must be a valid DNS name. Please read the naming convention described here:
+        // http://msdn.microsoft.com/en-us/library/azure/dd135715.aspx
+        AS::cloud_blob_container container = blobClient.get_container_reference(U("myd7349"));
+
+        container.create_if_not_exists();
+
+        // Set access permission
+        AS::blob_container_permissions permission;
+        permission.set_public_access(AS::blob_container_public_access_type::blob);
+        container.upload_permissions(permission);
+
+        // Upload a blob from a file
+        //utility::string_t filePath = U("D:\\学习笔记.txt");
+        utility::string_t filePath = U("D:\\20120929152243.dat");
+        concurrency::streams::istream inStream
+            = concurrency::streams::file_stream<std::uint8_t>::open_istream(filePath).get();
+        AS::cloud_block_blob blob = container.get_block_blob_reference(boost::filesystem::path(filePath).filename().native());
+        
+        // Now, start the uploading process. If we want to upload a block each time, then:
+        //   C#:     PutBlock, PutBlockList
+        //   Python: put_blob
+        //   C++:    upload_block, upload_block_list
+        ucout << U("Uploading file: ") << filePath << U("...");
+        {
+            utility::size64_t fileSize = boost::filesystem::file_size(filePath);
+            boost::progress_display prog(fileSize);
+            boost::progress_timer t;
+
+            while (fileSize >= reqOptions.stream_write_size_in_bytes()) {
+                blob.upload_from_stream(inStream, reqOptions.stream_write_size_in_bytes());
+                prog += reqOptions.stream_write_size_in_bytes();
+                fileSize -= reqOptions.stream_write_size_in_bytes();
+            }
+
+            if (fileSize > 0) {
+                blob.upload_from_stream(inStream, fileSize);
+                prog += fileSize;
+            }
+        }
+
+        // Close the stream
+        inStream.close().wait();
+    } catch (const AS::storage_exception &e) {
+        RETURN_ON_FAILURE("Azure storage exception");
+    } catch (const std::exception &e) {
+        RETURN_ON_FAILURE("Unknown exception");
+    }
 
     PAUSE();
     return EXIT_SUCCESS;
 }
+
+// References:
+// [Understanding Block Blobs and Page Blobs](http://msdn.microsoft.com/en-us/library/azure/ee691964.aspx)
+// [Windows Azure: 使用Blob的PutBlock实现大文件断点续传](http://www.cnblogs.com/lijiawei/archive/2013/01/18/2866756.html)
