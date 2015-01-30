d949d9a654a0da1f6d01eeb16c6772065fa96e30
myd7349 <myd7349@126.com>
2015-01-30 17:35:53 +0800

    Create passing_values_to_and_from_slots.cpp

---
+++ cpp/Boost/Signals2/passing_values_to_and_from_slots/passing_values_to_and_from_slots.cpp
@@ -0,0 +1,96 @@
+// 2015-01-30T16:33+08:00
+
+// This is an example that demonstrates how to pass values to and from slots.
+// Most code in this example is based on those code snippets in Boost.Signals2's
+// tutorial.
+
+#include <cassert>
+#include <iostream>
+
+#include <boost/signals2.hpp>
+
+#include "../../../common.h"
+#include "../../../uniform_typename/uniform_typename.h"
+
+double product(double x, double y) { return x * y; }
+double quotient(double x, double y) { assert(y != 0.0); return x / y; }
+double sum(double x, double y) { return x + y; }
+double difference(double x, double y) { return x - y; }
+
+namespace pass_values_to_slots {
+void print_args(double x, double y)
+{
+    std::cout << "The arguments are " << x << " and " << y << std::endl;
+}
+
+void print_sum(double x, double y)
+{
+    std::cout << "The sum is " << sum(x, y) << std::endl;
+}
+
+void print_product(double x, double y)
+{
+    std::cout << "The product is " << product(x, y) << std::endl;
+}
+
+void print_difference(double x, double y)
+{
+    std::cout << "The difference is " << difference(x, y) << std::endl;
+}
+
+void print_quotient(double x, double y)
+{
+    std::cout << "The quotient is " << quotient(x, y) << std::endl;
+}
+
+void test()
+{
+    boost::signals2::signal<void(double, double)> sig;
+
+    sig.connect(&print_args);
+    sig.connect(&print_sum);
+    sig.connect(&print_product);
+    sig.connect(&print_difference);
+    sig.connect(&print_quotient);
+
+    sig(5., 3.);
+}
+}
+
+namespace signal_return_values {
+void test()
+{
+    boost::signals2::signal<double(double, double)> sig;
+
+    sig.connect(&product);
+    sig.connect(&quotient);
+    sig.connect(&sum);
+    sig.connect(&difference);
+
+    // The default behaviour of a signal that has a return type is to call all 
+    // values and then return a boost::optional containing the result returned
+    // by the last slot called.
+    std::cout << TYPE_NAME(sig(5., 3.)) << std::endl; // class boost::optional<double>
+    std::cout << *sig(5, 3) << std::endl;
+}
+}
+
+// coalesce_values_returned_by_slots
+
+int main()
+{
+    SPLIT_LINE0();
+    pass_values_to_slots::test();
+
+    SPLIT_LINE0();
+    signal_return_values::test();
+
+    SPLIT_LINE0();
+
+
+    PAUSE();
+    return 0;
+}
+
+// References:
+// [Boost.Signals2 Tutorial](http://www.boost.org/doc/libs/1_57_0/doc/html/signals2/tutorial.html)
