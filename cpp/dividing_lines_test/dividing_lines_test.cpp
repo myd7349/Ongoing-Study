e13dd1182d6d2498f842bfe988f511fe34486811
myd7349 <myd7349@gmail.com>
2015-02-02 17:11:28 +0800

    Add dividing_lines_test

---
+++ cpp/dividing_lines_test/dividing_lines_test.cpp
@@ -0,0 +1,15 @@
+// 2015-02-02T15:06+08:00
+
+#define CONFIG_USING_BOOST (0)
+#include "../common.h"
+#include "../dividing_lines.h"
+
+int main()
+{
+    //DIVIDING_LINE(); // TODO
+    DIVIDING_LINE('+');
+    DIVIDING_LINE('=', 40);
+
+    PAUSE();
+    return 0;
+}
