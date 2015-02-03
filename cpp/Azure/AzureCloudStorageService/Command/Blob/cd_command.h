4ca447c525b9aa351d62f0bade6516c71979eb98
myd7349 <myd7349@126.com>
2015-02-03 10:01:24 +0800

    Create cd_command.h

---
+++ cpp/Azure/AzureCloudStorageService/Command/Blob/cd_command.h
@@ -0,0 +1,13 @@
+// 2015-02-03T08:39+08:00
+
+#ifndef CD_COMMAND_H_
+#define CD_COMMAND_H_
+
+#include "../command.h"
+
+#define CD_COMMAND_STR U("cd")
+
+class CdCommand : public Command {
+};
+
+#endif // CD_COMMAND_H_
