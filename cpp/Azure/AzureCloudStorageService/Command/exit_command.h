1f36ce40bed0bd9e110bdf0e09db604e7d97fc13
myd7349 <myd7349@126.com>
2015-02-03 09:56:41 +0800

    Create exit_command.h

---
+++ cpp/Azure/AzureCloudStorageService/Command/exit_command.h
@@ -0,0 +1,16 @@
+// 2015-01-19T17:26+08:00
+
+#ifndef EXIT_COMMAND_H_
+#define EXIT_COMMAND_H_
+
+#include "command.h"
+
+#define EXIT_COMMAND_STR U("exit")
+
+class ExitCommand : public Command {
+public:
+    virtual bool parse(const std::vector<utility::string_t> &/*vargs*/);
+    virtual bool run(azure::storage::cloud_storage_account &/*storage_account*/);
+};
+
+#endif // EXIT_COMMAND_H_
