1477d6847eab206fb1babaa434befcda272a95a5
myd7349 <myd7349@126.com>
2015-02-03 09:51:23 +0800

    Create azure_cloud_storage_service.h

---
+++ cpp/Azure/AzureCloudStorageService/azure_cloud_storage_service.h
@@ -0,0 +1,42 @@
+// 2015-01-28T08:46+08:00
+
+#ifndef AZURE_CLOUD_STORAGE_SERVICE_H_
+#define AZURE_CLOUD_STORAGE_SERVICE_H_
+
+#include <memory>
+#include <set>
+#include <unordered_map>
+
+#include <boost/core/noncopyable.hpp>
+
+#include <wascore/basic_types.h>
+#include <was/storage_account.h>
+
+struct AzureStorageAccountOptions {
+    utility::string_t account_name;
+    utility::string_t primary_access_key;
+    utility::string_t secondary_access_key;
+    utility::string_t end_point_suffix = U("core.windows.net");
+    bool use_https = false;
+    bool use_dev_storage = false;
+};
+
+class Command;
+
+// Azure cloud storage service wrapper class.
+class AzureCloudStorageService : public boost::noncopyable {
+public:
+    AzureCloudStorageService();
+    int run(const AzureStorageAccountOptions &storage_account_options);
+
+protected:
+    bool initialize(const AzureStorageAccountOptions &storage_account_options);
+    std::shared_ptr<Command> get_command(const utility::string_t &command);
+    void parse_command_and_dispatch();
+private:
+    azure::storage::cloud_storage_account storage_account_;
+    std::set<utility::string_t> available_commands_;
+    std::unordered_map<utility::string_t, std::shared_ptr<Command>> command_dispatcher_;
+};
+
+#endif // AZURE_CLOUD_STORAGE_SERVICE_H_
