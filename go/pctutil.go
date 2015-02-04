41b152770900c8da3f68b90bd720f8d6853c412d
myd7349 <myd7349@126.com>
2015-02-04 09:37:04 +0800

    Update pctutil.go

--- go/pctutil.go
+++ go/pctutil.go
@@ -1,5 +1,4 @@
 // 2015-01-07T08:59+08:00
-
 // Percent-encoding/decoding utility.
 //
 // Sometimes, when I downloaded something via my firefox, I may got a file with
@@ -14,12 +13,11 @@
 // However, I am so lazy that I never start this work.
 //
 // I also considered to write such a program with C. And libcurl also provides us two
-// functions to do this work: curl_easy_escape, curl_easy_unescape. However, it is a little 
-// difficult to deal with different character encodings in C(may be libiconv is a good choice). 
+// functions to do this work: curl_easy_escape, curl_easy_unescape. However, it is a little
+// difficult to deal with different character encodings in C(may be libiconv is a good choice).
 // And you know, I am lazy, so...
 //
 // Recently, I began to learn Go. So, I wrote this program as a practice.
-
 package main
 
 import (
@@ -34,7 +32,6 @@ import (
 
 func unescape(s string) string {
 	normname, err := url.QueryUnescape(s)
-
 	if err == nil {
 		return normname
 	} else {
@@ -46,12 +43,9 @@ func unescape(s string) string {
 func main() {
 	// -- Parse command line arguments.
 	var path, method string
-
 	flag.StringVar(&path, "f", "", "Specify the file name or path name to be processed")
 	flag.StringVar(&method, "m", "unescape", "Specify the operation: \"escape\" or \"unescape\"")
-
 	flag.Parse()
-
 	// -- Decide the action we should take according to the "-m" option.
 	var convert = func(string) string { return "" }
 	switch method {
@@ -63,32 +57,26 @@ func main() {
 		fmt.Println("Unknown operation:", method)
 		return
 	}
-
 	// -- Now, do something meaningful to make the life a little more easier.
 	if len(path) == 0 {
 		// If path name is empty, then read string from stdin.
 		stdin := bufio.NewReader(os.Stdin)
 		var line string
 		var err error
-
 		for {
-			line, err = stdin.ReadString('\n')
-			if err == nil {
+			if line, err = stdin.ReadString('\n'); err == nil {
 				fmt.Println(convert(strings.TrimSuffix(line, "\r\n")))
 			}
 		}
 	} else {
 		// Otherwise, process the target file/path name.
-		_, err := os.Stat(path)
-		if err != nil {
+		if _, err := os.Stat(path); err != nil {
 			fmt.Printf("File or path \"%s\" doesn't exist.\n", path)
 			return
 		}
-
 		// We just process the last pathname component.
 		head, tail := filepath.Split(path)
 		newpath := filepath.Join(head, convert(tail))
-
 		if newpath != path {
 			os.Rename(path, newpath)
 			fmt.Println(path, "->", newpath)
