5c7d6338f17c777b5614932d42bdab55f11bb08d
myd7349 <myd7349@126.com>
2015-02-26 14:57:01 +0800

    Add comment

--- c/PThreads/assign_thread_identity/assign_thread_identity_v1.c
+++ c/PThreads/assign_thread_identity/assign_thread_identity_v1.c
@@ -1,7 +1,17 @@
 /* 2015-01-16T16:39+08:00 */
 
-/* gcc assign_thread_identity_v1.c -pthread -Wall */
+/* 
+My environment: CentOS 6 x86_64 + GCC 4.4.7
+Try:
+1. gcc assign_thread_identity_v1.c -pthread -Wall 
+   There will be two warnings.
+2. g++ assign_thread_identity_v1.c -pthread -Wall 
+   There will be an compile-time error:
+       assign_thread_identity_v1.c: In function ‘void* routine(void*)’:
+       assign_thread_identity_v1.c:23: error: cast from ‘void*’ to ‘int’ loses precision
+*/
 
+#include <assert.h>
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -11,8 +21,8 @@
 
 static void *routine(void *arg)
 {
-    /* When I run this program on CentOS 6 x64, I got a warning here:
-     * warning: cast from pointer to integer of different size
+    /* On CentOS 6 x86_64 with GCC 4.4.7: 
+     * warning: cast from pointer to integer of different size 
      */
     int thrd_id = (int)arg;
 
@@ -26,16 +36,28 @@ int main(void)
     pthread_t thrds[NUM_OF_THRDS];
     int i;
     int rc;
+    void *res = NULL;
 
     for (i = 0; i < NUM_OF_THRDS; ++i) {
-        /* When I run this program on CentOS 6 x64, I got a warning here:
+        /* On CentOS 6 x86_64 with GCC 4.4.7:
          * warning: cast to pointer from integer of different size
          */
         rc = pthread_create(thrds + i, NULL, &routine, (void *)i);
+        assert(0 == rc);
         if (rc != 0) {
             fprintf(stderr, "Failed to create thread #%d.\n", i);
             exit(EXIT_FAILURE);
         }
+    }
+
+    for (i = 0; i < NUM_OF_THRDS; ++i) {
+        rc = pthread_join(thrds[i], &res);
+        assert(0 == rc);
+        if (rc != 0) {
+            fprintf(stderr, "Failed to join thread #%d.\n", i);
+            exit(EXIT_FAILURE);        
+        }
+        free(res);
     }
 
     printf("Hello, world!");
@@ -45,5 +67,7 @@ int main(void)
 
 /*
 References:
-Linux Pthread library, thread arguments: http://stackoverflow.com/questions/10871427/linux-pthread-library-thread-arguments
+[error: cast from 'void*' to 'int' loses precision](http://stackoverflow.com/questions/1640423/error-cast-from-void-to-int-loses-precision)
+[is it necessary to call pthread_join()](http://stackoverflow.com/questions/10600250/is-it-necessary-to-call-pthread-join)
+[Linux Pthread library, thread arguments](http://stackoverflow.com/questions/10871427/linux-pthread-library-thread-arguments)
 */
