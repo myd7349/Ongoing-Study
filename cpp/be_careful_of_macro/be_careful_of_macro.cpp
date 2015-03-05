93a6e8bb4ddcb80f5044ec4ce6b00d2d0616060b
myd7349 <myd7349@gmail.com>
2015-03-05 11:19:17 +0800

    Add be_careful_of_macro

---
+++ cpp/be_careful_of_macro/be_careful_of_macro.cpp
@@ -0,0 +1,21 @@
+// 2014-06-24T15:28+08:00
+// http://stackoverflow.com/questions/5416414/signed-unsigned-comparisons
+// http://www.zhihu.com/question/25870045/answer/31609883
+#include <algorithm>
+#include <iostream>
+
+#define MIN(a, b) ((a)<(b)?(a):(b))
+
+#ifdef min
+#  undef min
+#endif
+
+int main()
+{
+    std::cout << MIN(-1, 456U) << std::endl; // 456
+    std::cout << MIN(-1L, 456U) << std::endl; // 456
+    std::cout << std::min<int>(-1, 456U) << std::endl; // -1
+    std::cout << std::min<unsigned int>(-1, 456U) << std::endl; // 456
+
+    return 0;
+}
