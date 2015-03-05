b725bdd38e86e6867f3a4f2c9469b448fd015a62
myd7349 <myd7349@gmail.com>
2015-03-05 11:10:36 +0800

    Finish dynamic_cast_test

--- cpp/RTTI/dynamic_cast_test/dynamic_cast_test.cpp
+++ cpp/RTTI/dynamic_cast_test/dynamic_cast_test.cpp
@@ -3,6 +3,11 @@
 #include <iostream>
 #include <typeinfo>
 
+// To do a dynamic_cast:
+//   1. The source type must be pointer or reference to class. And this class
+//      is polymorphic.
+//   2. The target type must be pointer or reference to class, or a pointer of
+//      type void (that is, "void *").
 void test_0()
 {
 #if 0
@@ -10,12 +15,19 @@ void test_0()
 
     // error: cannot dynamic_cast '& i' (of type 'int*') to type 'double*'
     // (target is not pointer or reference to class)|
-    double d = *dynamic_cast<double *>(&i);
+    double *d = dynamic_cast<double *>(&i);
 #endif
 }
 
 void test_1()
 {
+#if 0
+    // error: cannot dynamic_cast 'nullptr' (of type 'std::nullptr_t') to type 'void*'
+    // (source is not a pointer)|
+    dynamic_cast<void *>(nullptr);
+    dynamic_cast<void *>(NULL);
+#endif
+
 #if 0
     int i = 42;
 
@@ -29,7 +41,7 @@ void test_2()
 {
 #if 0
     // error: cannot dynamic_cast '& foo' (of type 'class main()::Foo*') to type 'class main()::Bar*'
-    // (source type is not polymorphic)|
+    // (dynamic_cast<World *>(&hello)source type is not polymorphic)|
     class Foo {};
     class Bar {};
 
@@ -38,6 +50,7 @@ void test_2()
 #endif
 }
 
+// Downcasting test
 void test_3()
 {
     class Hello {
@@ -45,14 +58,29 @@ void test_3()
     };
     class World {
     };
+
     Hello hello;
-    World *world = dynamic_cast<World *>(&hello);
-    assert(nullptr == world);
+    assert(nullptr == dynamic_cast<World *>(&hello));
+
+    void *pv = dynamic_cast<void *>(&hello);
+    assert(pv != nullptr);
+
+#if 0
+    // Oops! Remember test_1?
+    assert(dynamic_cast<Hello *>(pv) != nullptr);
+    assert(nullptr == dynamic_cast<World *>(pv));
+#endif
+
     try {
         dynamic_cast<World &>(hello);
     } catch (const std::bad_cast &e) {
         std::cerr << e.what() << std::endl;
     }
+}
+
+// TODO: Side cast
+void test_4()
+{
 }
 
 int main()
@@ -61,8 +89,12 @@ int main()
     test_1();
     test_2();
     test_3();
+    test_4();
 
     return 0;
 }
 // References:
+// [dynamic_cast](http://en.wikipedia.org/wiki/Dynamic_cast)
 // [dynamic_cast conversion](http://en.cppreference.com/w/cpp/language/dynamic_cast)
+// [cv (const and volatile) type qualifiers](http://en.cppreference.com/w/cpp/language/cv)
+// [Downcasting](http://en.wikipedia.org/wiki/Downcasting)
