31d44e2c96f55d6fa864aa284098065c0366dfd2
myd7349 <myd7349@gmail.com>
2015-03-11 13:50:17 +0800

    Add copy_a_file_in_a_sane_way

---
+++ cpp/copy_a_file_in_a_sane_way/copy_a_file_in_a_sane_way.cpp
@@ -0,0 +1,30 @@
+// 2015-03-11T13:39+08:00
+// [Copy a file in an sane, safe and efficient way](http://stackoverflow.com/questions/10195343/copy-a-file-in-an-sane-safe-and-efficient-way)
+#include <fstream>
+#include <iostream>
+
+#include "../common.h"
+
+int main(int argc, char *argv[])
+{
+    if (argc != 3) {
+        std::cerr << "Usage: " << GetProgName(argv[0]) << " srcfile destfile\n";
+        return EXIT_FAILURE;
+    }
+
+    std::ifstream src_file(argv[1], std::ios::binary);
+    if (!src_file) {
+        std::cerr << "Opening source file \"" << argv[1] << "\" failed!\n";
+        return EXIT_FAILURE;
+    }
+
+    std::ofstream dest_file(argv[2], std::ios::binary);
+    if (!dest_file) {
+        std::cerr << "Opening destination file \"" << argv[2] << "\" failed!\n";
+        return EXIT_FAILURE;
+    }
+
+    dest_file << src_file.rdbuf();
+
+    return 0;
+}
