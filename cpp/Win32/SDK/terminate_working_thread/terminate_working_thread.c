874eecde0f6fa6c6e87c617a61d8b5c04aa0e9ed
myd7349 <myd7349@gmail.com>
2015-03-12 08:57:33 +0800

    Add terminate_working_thread

---
+++ cpp/Win32/SDK/terminate_working_thread/terminate_working_thread.c
@@ -0,0 +1,52 @@
+// 2015-03-12T08:25+08:00
+#include <stdio.h>
+#include <stdlib.h>
+
+#include <conio.h>
+#include <process.h>
+#include <tchar.h>
+#include <windows.h>
+
+static BOOL g_bWorking;
+
+static unsigned int __stdcall WorkingRoutine(void *arg)
+{
+    while (g_bWorking) {
+        _puttc(_T('.'), stdout);
+        Sleep(200);
+    }
+
+    return 0;
+}
+
+int _tmain(int argc, _TCHAR **argv)
+{
+    HANDLE hWorkingThread = (HANDLE)_beginthreadex(
+        NULL, 
+        0, 
+        WorkingRoutine,
+        NULL,
+        CREATE_SUSPENDED,
+        NULL);
+
+    if (NULL == hWorkingThread) {
+        _ftprintf(stderr, _T("Creating working thread failed!\n"));
+        return EXIT_FAILURE;
+    }
+
+    g_bWorking = TRUE;
+    ResumeThread(hWorkingThread);
+
+    SetConsoleTitle(_T("Hit any key to terminate the working thread!"));
+
+    while (!kbhit()) {
+    }
+
+    InterlockedExchange(&g_bWorking, FALSE);
+    WaitForSingleObject(hWorkingThread, INFINITE);
+
+    _ftprintf(stdout, "Goodbye!\n");
+
+    _tsystem(_T("pause"));
+    return 0;
+}
