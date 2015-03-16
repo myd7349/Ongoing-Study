12d4383004d3a9bf9b20078f87a6c5174d228598
myd7349 <myd7349@gmail.com>
2015-03-16 10:11:58 +0800

    Add delete_container and delete_block_blob

--- cpp/Azure/AzureCloudStorageService/Command/Blob/rm_command.cpp
+++ cpp/Azure/AzureCloudStorageService/Command/Blob/rm_command.cpp
@@ -2,22 +2,40 @@
 
 #include "rm_command.h"
 
+#include "../../azure_cloud_storage_service.h"
+
+void delete_container(azure::storage::cloud_blob_container &container)
+{
+    container.delete_container_if_exists();
+}
+
+void delete_block_blob(azure::storage::cloud_blob_container &container, 
+    const utility::string_t &blob_name)
+{
+    auto blob = container.get_block_blob_reference(blob_name);
+    blob.delete_blob_if_exists();
+}
+
 RmCommand::RmCommand()
 {
-
 }
 
 bool RmCommand::parse(const std::vector<utility::string_t> &vargs)
 {
-    return true;
+    return parse_helper(vargs, RM_COMMAND_STR);
 }
 
 bool RmCommand::run(AzureCloudStorageService *storage_service)
 {
+    if (container_name_.empty()) {
+        if (storage_service->current_container_.is_valid()) {
+        } else {
+        }
+    } else {
+    }
     return true;
 }
 
 void RmCommand::help() const
 {
-
 }
