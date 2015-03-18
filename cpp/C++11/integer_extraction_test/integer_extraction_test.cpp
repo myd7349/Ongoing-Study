d2c6326711d2e600680b13d6417818b6bd17c4db
myd7349 <myd7349@gmail.com>
2015-03-18 11:13:44 +0800

    Update integer_extraction_test.cpp

--- cpp/C++11/integer_extraction_test/integer_extraction_test.cpp
+++ cpp/C++11/integer_extraction_test/integer_extraction_test.cpp
@@ -13,28 +13,28 @@ int main()
     //
     // ISO C++11 27.7.2.2.2 Arithmetic extractors
     // As in the case of the inserters, these extractors
-    // depend on the locale¨s num_get<> (22.4.2.1) object
+    // depend on the locale's num_get<> (22.4.2.1) object
     // to perform parsing the input stream data.
     // ISO C++11 22.4.2.1.2
     // Stage 3: The sequence of chars accumulated in
     // stage 2 (the field) is converted to a numeric
     // value by the rules of one of the functions
     // declared in the header <cstdlib>:
-    // ！ For a signed integer value, the function strtoll.
-    // ！ For an unsigned integer value, the function strtoull.
-    // ！ For a floating-point value, the function strtold.
+    // -- For a signed integer value, the function strtoll.
+    // -- For an unsigned integer value, the function strtoull.
+    // -- For a floating-point value, the function strtold.
     // The numeric value to be stored can be one of:
-    // ！ zero, if the conversion function fails to convert the
+    // -- zero, if the conversion function fails to convert the
     //   entire field. ios_base::failbit is assigned to err.
     //   (In ISO C++98, there is no this sentence.)
-    // ！ the most positive representable value, if the field
+    // -- the most positive representable value, if the field
     //   represents a value too large positive to be represented
     //   in val. ios_base::failbit is assigned to err.
-    // ！ the most negative representable value or zero for
+    // -- the most negative representable value or zero for
     //   an unsigned integer type, if the field represents a value
     //   too large negative to be represented in val. ios_base::failbit
     //   is assigned to err.
-    // ！ the converted value, otherwise.
+    // -- the converted value, otherwise.
     while (!(std::cin >> i)) {
         std::cout << i << std::endl;
         std::cin.clear();
