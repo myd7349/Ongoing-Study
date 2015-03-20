8b8db4a05248fb1b7e662b30ed02f2df55781ae3
myd7349 <myd7349@gmail.com>
2015-03-20 16:48:02 +0800

    Add sync_pub_sub

---
+++ c/ZeroMQ/zguide_examples/sync_pub_sub/syncpub/syncpub.c
@@ -0,0 +1,50 @@
+// 2015-01-12T13:59+08:00
+// Synchronized publisher
+
+#include "../zhelpers.h"
+
+#define SUBSCRIBERS_EXPECTED (3) // We wait for 10 subscribers
+
+int main(void)
+{
+    void *context = zmq_ctx_new();
+
+    // Socket to talk to clients
+    void *publisher = zmq_socket(context, ZMQ_PUB);
+
+    int sndhwm = 1100000;
+    zmq_setsockopt(publisher, ZMQ_SNDHWM, &sndhwm, sizeof(int));
+
+    zmq_bind(publisher, "tcp://*:5561");
+
+    // Socket to receive signals
+    void *syncservice = zmq_socket(context, ZMQ_REP);
+    zmq_bind(syncservice, "tcp://*:5562");
+
+    // Get synchronization from subscribers
+    printf("Waiting for subscribers\n");
+    int subscribers = 0;
+    while (subscribers < SUBSCRIBERS_EXPECTED) {
+        // - wait for synchronization request
+        char *string = s_recv(syncservice);
+        free(string);
+        // - send synchronization reply
+        s_send(syncservice, "");
+        subscribers++;
+    }
+
+    // Now broadcast exactly 1M updates followed by END
+    printf("Broadcasting messages\n");
+    int update_nbr;
+    for (update_nbr = 0; update_nbr < 1000000; update_nbr++) {
+        s_send(publisher, "Rhubarb");
+    }
+
+    s_send(publisher, "END");
+
+    zmq_close(publisher);
+    zmq_close(syncservice);
+    zmq_ctx_destroy(context);
+
+    return 0;
+}
---
+++ c/ZeroMQ/zguide_examples/sync_pub_sub/syncsub/syncsub.c
@@ -0,0 +1,48 @@
+// 2015-01-12T13:59+08:00
+// Synchronized subscriber
+
+#include "../zhelpers.h"
+
+int main(void)
+{
+    void *context = zmq_ctx_new();
+
+    // First, connect our subscriber socket
+    void *subscriber = zmq_socket(context, ZMQ_SUB);
+    zmq_connect(subscriber, "tcp://localhost:5561");
+    zmq_setsockopt(subscriber, ZMQ_SUBSCRIBE, "", 0);
+
+    // 0MQ is so fast, we need to wait a while...
+    Sleep(1);
+
+    // Second, synchronize with publisher
+    void *syncclient = zmq_socket(context, ZMQ_REQ);
+    zmq_connect(syncclient, "tcp://localhost:5562");
+
+    // - send a synchronization request
+    s_send(syncclient, "");
+
+    // Wait for synchronization reply
+    char *string = s_recv(syncclient);
+    free(string);
+
+    // Third, get our updates and report how many we got
+    int update_nbr = 0;
+    while (1) {
+        char *string = s_recv(subscriber);
+        if (strcmp(string, "END") == 0) {
+            free(string);
+            break;
+        }
+        free(string);
+        update_nbr++;
+    }
+
+    printf("Received %d updates\n", update_nbr);
+
+    zmq_close(subscriber);
+    zmq_close(syncclient);
+    zmq_ctx_destroy(context);
+
+    return 0;
+}
