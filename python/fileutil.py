d62a7ac1bb0c58d033f8a71895373812a2156532
myd7349 <myd7349@gmail.com>
2015-03-24 09:55:59 +0800

    Add replace_ext

--- python/fileutil.py
+++ python/fileutil.py
@@ -7,18 +7,34 @@ import functools
 import io
 import os
 
-def fp_size(fp):
-    '''Get the size of given file object.'''
-    if not all(map(functools.partial(hasattr, fp), ['read', 'fileno', 'seek', 'tell'])):
-        raise ValueError('Invalid file object')
+def file_size(f):
+    '''Get the size of given file.
+
+    f: File name or an opened file object
+    '''
+    
+    if not all(map(functools.partial(hasattr, f),
+                   ['read', 'fileno', 'seek', 'tell'])):
+        return os.stat(f).st_size
 
     try:
-        return os.fstat(fp.fileno()).st_size
+        return os.fstat(f.fileno()).st_size
     except io.UnsupportedOperation:
-        prev_pos = fp.tell()
-        size = fp.seek(0, os.SEEK_END)
-        fp.seek(prev_pos, os.SEEK_SET)
+        prev_pos = f.tell()
+        size = f.seek(0, os.SEEK_END)
+        f.seek(prev_pos, os.SEEK_SET)
         return size
+
+def replace_ext(file, new_ext):
+    '''Produce a new file name based on input file name with its extension
+    replaced with a new extension.
+
+    file: Source file name
+    new_ext: The new extension to be used
+    '''
+
+    root, ext = os.path.splitext(file)
+    return root + new_ext if ext else file
 
 if __name__ == '__main__':
     import unittest
@@ -26,14 +42,15 @@ if __name__ == '__main__':
     class TestFPSize(unittest.TestCase):
         def test_regular_file(self):
             test_file = 'fpsize_test.dat'
+            data = b'Hello, world'
             try:
                 with open(test_file, 'wb') as fp:
-                    data = b'Hello, world'
                     fp.write(data)
                     fp.flush()
 
-                    self.assertEqual(fp_size(fp), len(data))
+                    self.assertEqual(file_size(fp), len(data))
             finally:
+                self.assertEqual(file_size(test_file), len(data))
                 os.remove(test_file)
 
         def test_bytes_buffer(self):
@@ -43,10 +60,18 @@ if __name__ == '__main__':
             self.assertRaises(io.UnsupportedOperation, pseudo_file.fileno)
             self.assertEqual(pseudo_file.read(5), b'Hello')
             self.assertEqual(pseudo_file.tell(), 5)
-            self.assertEqual(fp_size(pseudo_file), len(buffer))
+            self.assertEqual(file_size(pseudo_file), len(buffer))
             self.assertEqual(pseudo_file.tell(), 5)
+            self.assertEqual(pseudo_file.read(), b', world!')
 
+    class TestReplaceExt(unittest.TestCase):
+        def test(self):
+            file1 = 'a.out'
+            self.assertEqual(replace_ext(file1, '.exe'), 'a.exe')
 
+            file2 = '.cshrc'
+            self.assertEqual(replace_ext(file2, '.conf'), file2)
+            
     unittest.main()
     
 # References:
