051cbe37ea8992c60c94c342aacd981ca3d767c0
myd7349 <myd7349@gmail.com>
2015-04-02 14:38:37 +0800

    Add Str2DoubleTest

---
+++ cpp/Qt/Str2DoubleTest/Str2DoubleTest.pro
@@ -0,0 +1,18 @@
+#-------------------------------------------------
+#
+# Project created by QtCreator 2014-07-15T16:34:41
+#
+#-------------------------------------------------
+
+QT       += core
+
+QT       -= gui
+
+TARGET = Str2DoubleTest
+CONFIG   += console
+CONFIG   -= app_bundle
+
+TEMPLATE = app
+
+
+SOURCES += main.cpp
---
+++ cpp/Qt/Str2DoubleTest/main.cpp
@@ -0,0 +1,33 @@
+﻿// 2014-07-15T17:34+08:00
+// http://zhidao.baidu.com/question/199168895602898645.html
+#include <QtCore/QCoreApplication>
+#include <QtCore/QDebug>
+#include <QtCore/QString>
+#include <QtCore/QTextStream>
+
+#include <iomanip>
+#include <iostream>
+
+int main(int argc, char *argv[])
+{
+    QCoreApplication a(argc, argv);
+
+    QString num("1234567890");
+    double d = num.toDouble();
+
+    // QTextStream 定义了一些manipulators
+    qDebug() << fixed << qSetRealNumberPrecision(0) << d << endl;
+    qDebug() << QString::number(d, 'f', 0);
+
+    QTextStream os(stderr);
+    os << fixed; // os.setRealNumberNotation(QTextStream::FixedNotation);
+    os.setRealNumberPrecision(0);
+    os << d << '\n';
+    os.flush(); // Why stderr still need to flush?
+
+    std::cout << d << std::endl;
+    std::cout << std::fixed << std::setprecision(0)
+              << d << std::endl;
+
+    return a.exec();
+}
