8df5ce8fab4c90cccf2cfe3917230c8a09e1926c
myd7349 <myd7349@gmail.com>
2015-04-10 11:38:21 +0800

    Add get_bits_of_int_v4

--- c/sizeof_int/sizeof_int.c
+++ c/sizeof_int/sizeof_int.c
@@ -1,8 +1,9 @@
 /*
 2015-04-10T10:44+08:00
 http://www.zhihu.com/question/29402649
-
 */
+#include <limits.h>
+#include <math.h>
 #include <stddef.h>
 #include <stdio.h>
 
@@ -31,12 +32,17 @@ http://stackoverflow.com/questions/1219199/size-of-a-datatype-without-using-size
 #define SIZEOF(type) ((size_t)(1 + (type *)0))
 #define get_bits_of_int_v3() (SIZEOF(int) * 8)
 
+size_t get_bits_of_int_v4(void)
+{
+    return (size_t)log2(UINT_MAX);
+}
+
 int main()
 {
     printf("%u\n", get_bits_of_int_v1());
     printf("%u\n", get_bits_of_int_v2());
     printf("%u\n", get_bits_of_int_v3());
-
+    printf("%u\n", get_bits_of_int_v4());
     printf("%u\n", SIZEOF(double));
 
     return 0;
