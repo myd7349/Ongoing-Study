9aa75be17fff6a9980fe197b703beb7dc9a820a4
myd7349 <myd7349@gmail.com>
2015-04-25 16:20:30 +0800

    Add PUB-SUB test

---
+++ python/ZeroMQ/pyzmq/PUB-SUB/1v1/subscriber.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+# 2014-12-25T17:02+08:00
+
+# http://blogs.ejb.cc/archives/5556/zeromq-via-c-sharp-a-communication-pattern
+
+import zmq
+
+context = zmq.Context()
+socket = context.socket(zmq.SUB)
+socket.connect('tcp://localhost:5556')
+
+socket.setsockopt_string(zmq.SUBSCRIBE, '')
+
+while True:
+    print(socket.recv_string())
---
+++ python/ZeroMQ/pyzmq/PUB-SUB/nv1/publisher_kiwi.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+# 2014-12-25T16:55+08:00
+
+# http://blogs.ejb.cc/archives/5556/zeromq-via-c-sharp-a-communication-pattern
+
+import zmq
+
+context = zmq.Context()
+socket = context.socket(zmq.PUB)
+socket.bind('tcp://*:5557')
+
+i = 0
+while True:
+    msg = 'Kiwi   {}'.format(i)
+    print(msg)
+    socket.send_string(msg)
+    i += 1
