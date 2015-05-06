97492e9a423514d0ab95e9efdc016b43f06b7c09
myd7349 <myd7349@gmail.com>
2015-05-06 17:23:20 +0800

    Add MedianFiltering

---
+++ algorithm/math/MedianFiltering/medfilt.hpp
@@ -0,0 +1,19 @@
+// 2015-05-06T15:58+08:00
+
+// 1-D median filtering to remove baseline wander for ECG.
+#ifndef MEDFILT_H_INCLUDED
+#define MEDFILT_H_INCLUDED
+
+template <typename T>
+class MedianFilter
+{
+public:
+private:
+};
+
+#endif // MEDFILT_H_INCLUDED
+
+// References:
+// [1] MATLAB: doc medfilt2
+// [2] 快速中值滤波在滤除心电信号基线漂移中的应用
+// [3] 张勇，张萍，王介生，心电图中滤除基线漂移的研究
