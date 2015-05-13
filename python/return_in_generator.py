10253066a106d82e57aa990f8a6e2e95cafca0f8
myd7349 <myd7349@gmail.com>
2015-05-13 10:03:47 +0800

    Update return_in_generator.py

--- python/return_in_generator.py
+++ python/return_in_generator.py
@@ -10,7 +10,7 @@ def main():
     #---------------------------------------------------------------------------
     # Suppose that we got a C-style integer array that contains 10 integers:
     #   int data[10] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
-    # so how is this array stored in memory?
+    # then how is this array stored in memory?
     
     data_range = range(10)
 
@@ -33,6 +33,7 @@ def main():
     #---------------------------------------------------------------------------
     # unpack_data_v0 itself is a function that returns an iterable object.
     # unpack_data_v1 itself is a generator function.
+    # Besides, there is no difference between them.
     def unpack_data_v0():
         return struct.iter_unpack('=i', data)
     def unpack_data_v1():
@@ -47,8 +48,38 @@ def main():
     print()
 
     #---------------------------------------------------------------------------
-    # To be continued...
+    # However, it is not always the case. For example, in a generator function
+    # we can only use `yield from`, not `return`.
+    def unpack_data_v3(do_real_work = True):
+        if do_real_work:
+            return struct.iter_unpack('=i', data)
+        else:
+            for i in (1, 1, 2, 3, 5, 8, 13, 21):
+                yield i,
 
+    def unpack_data_v4(do_real_work = True):
+        if do_real_work:
+            yield from struct.iter_unpack('=i', data)
+        else:
+            for i in (1, 1, 2, 3, 5, 8, 13, 21):
+                yield i,      
+
+    for i in unpack_data_v3():
+        print(*i, end = ' ')
+    print()
+    
+    for i in unpack_data_v4():
+        print(*i, end = ' ')
+    print()
+
+    for i in unpack_data_v3(False):
+        print(*i, end = ' ')
+    print()
+    
+    for i in unpack_data_v4(False):
+        print(*i, end = ' ')
+    print()
+    
 if __name__ == '__main__':
     main()
 
