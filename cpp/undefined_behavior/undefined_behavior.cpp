8aea740f4708cc6bd0d0bc1b318a6f99c54f93e1
myd7349 <myd7349@gmail.com>
2015-05-30 21:56:11 +0800

    Add undefined_behavior

---
+++ cpp/undefined_behavior/undefined_behavior.cpp
@@ -0,0 +1,19 @@
+// 2015-05-30T21:49+08:00
+// List of undefined behaviors in C++.
+
+// 1. dereferencing a null pointer.
+//    An example from Effective C++ 3E/Introduction/Terminology
+//        int *p = NULL;
+//        std::cout << *p;
+//
+
+// 2. referring an invalid array index
+//    An example from Effective C++ 3E/Introduction/Terminology
+//        char name[] = "Darla";
+//        char c = name[10];
+
+
+int main()
+{
+    return 0;
+}
