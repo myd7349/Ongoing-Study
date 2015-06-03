47b75ce2274d2822e07871827596e56edabdce3a
myd7349 <myd7349@gmail.com>
2015-06-03 09:30:49 +0800

    Update ucps.py

--- python/ucps.py
+++ python/ucps.py
@@ -5,19 +5,24 @@
 
 # http://www.zhihu.com/question/26772647
 
+# 2015-06-03T09:03+08:00
+# http://www.zhihu.com/question/26921730
+
 def ucps_to_str(cps):
-    '''Translate an unicode code point sequence like this:
-        \u6d77\u9614\u5929\u7a7a
+    '''Translate an unicode code point sequence LITERAL like this:
+        r'\u6d77\u9614\u5929\u7a7a'
     into a str object.'''
     cp_list = cps.replace(r'\u', ' ').split()
     return ''.join(chr(int(cp, 16)) for cp in cp_list)
 
 def str_to_ucps(s):
-    '''Translate a str object into an unicode code point sequence.'''
+    '''Translate a str object into an unicode code point sequence LITERAL.'''
     return ''.join(hex(ord(c)).replace('0x', r'\u') for c in s)
 
 if __name__ == '__main__':
     cps = str_to_ucps('海阔天空')
     print(cps)
     print(ucps_to_str(cps))
+    print('{!r}'.format(cps))
+    print(repr(cps))
 
