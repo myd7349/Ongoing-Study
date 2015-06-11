7ee969edecfd983c4838c265441fc7a855b3f03d
myd7349 <myd7349@gmail.com>
2015-06-11 18:25:38 +0800

    Add _load_config

--- python/dcmutil.py
+++ python/dcmutil.py
@@ -30,9 +30,34 @@ _errors = {
     }
 
 _prog = fileutil.file_title(sys.argv[0])
-_generic_options = (
-    {'dest': '--config', 'help': 'configuration file that stores FTP/{} options'.format(_prog)}
+
+_generic_options_group = (
+    'Generic',
+    (['--config'], {'help': 'configuration file that stores FTP/{} options'.format(_prog)}),
+    (['--errcodes'], {'action': 'store_true', 'help': 'list all error codes this program may return'}),
+    (['--source'], {'required': True, 'help': 'source data file to be converted'}),
+    (['--target'], {'required': True, 'help': 'target DICOM file name'}),
+    (['--type'], {'choices': ['ECG', 'FECG'], 'default': 'ECG', 'help': 'source data type: "ECG" for normal 12-Lead ECG, and "FECG" for Foetus ECG'}),
+    (['--criteria'], {'help': 'criteria argument'}),
+    (['--template'], {'help': 'template file name'}),
+    (['-h', '--help'], {'action': 'help', 'help': 'show this help message and exit'}),
+    (['-v', '--version'], {'action': 'version', 'version': ' '.join((_prog, __version__, 'by', __author__))}),
     )
+
+_ftp_options_group = (
+    'FTP',
+    (['--host'], {'help': 'hostname to connect to'}),
+    (['--port'], {'help': 'port to connect to'}),
+    (['--user'], {'help': 'login user name'}),
+    (['--passwd'], {'help': 'login password'}),
+    )
+
+_prog_options_group = (
+    _prog,
+    (['--info'], {'help': 'configuration file that stores additional patient information'}),
+    (['--log'], {'help': 'log file name'}),
+    )
+
 
 def _parse_args():
     parser = argparse.ArgumentParser(prog=_prog,
@@ -41,34 +66,18 @@ def _parse_args():
                                      'If "--host" is provided, then target DICOM file will be sent to this host, otherwise \'--target\' stands for a path on local drive.',
                                      formatter_class=argparse.RawDescriptionHelpFormatter,
                                      add_help=False)
-    
-    generic_options = parser.add_argument_group('Generic options')
-    generic_options.add_argument(dest='--config', help='configuration file that stores FTP/{} options'.format(parser.prog))
-    generic_options.add_argument('-h', '--help', action='help', help='show this help message and exit')
-    generic_options.add_argument('-v', '--version', action='version', version=' '.join((fileutil.file_title('%(prog)s'), __version__, 'by', __author__)))
 
-    ftp_options = parser.add_argument_group('FTP options')
-    ftp_options.add_argument('--host', default='', help='hostname to connect to')
-    ftp_options.add_argument('--port', default='', help='port to connect to')
-    ftp_options.add_argument('--user', default = '', help='login user name')
-    ftp_options.add_argument('--passwd', default = '', help='login password')
-
-    dcmutil_options = parser.add_argument_group(parser.prog + ' options')
-    dcmutil_options.add_argument('--source', required=True, help='source data file to be converted')
-    dcmutil_options.add_argument('--target', required=True, help='target DICOM file name')
-    dcmutil_options.add_argument('--type', choices=['ECG', 'FECG'], help='source data type: "ECG" for normal 12-Lead ECG, and "FECG" for Foetus ECG')
-    
-    dcmutil_options.add_argument('--template', help='template file name')
-    dcmutil_options.add_argument('--info', help='configuration file that stores additional patient information')
-    dcmutil_options.add_argument('--criteria', help='criteria argument')
-    dcmutil_options.add_argument('--log', help='log file name')
-    dcmutil_options.add_argument('--errcodes', action='store_true', help='list all error codes this program may return')
+    for group in (_generic_options_group, _ftp_options_group, _prog_options_group):
+        arg_group = parser.add_argument_group(group[0] + ' options')
+        for args, kwargs in group[1:]:
+            arg_group.add_argument(*args, **kwargs)
 
     if len(sys.argv) <= 1:
         parser.print_help()
         _report_error(ErrorCode.ok)
         
     return parser.parse_args()
+
 
 def _report_error(error_code):
     assert error_code in _errors
@@ -77,10 +86,38 @@ def _report_error(error_code):
         print('[{:d}] {}'.format(error_code, _errors[error_code]), file=sys.stderr)
     sys.exit(error_code)
 
-def main():
+
+def _load_config(args):
+    if not args.config:
+        return
+
     import configparser
     import os.path
+
+    config = configparser.ConfigParser()
+    config.optionxform = str
+
+    groups = (_ftp_options_group, _prog_options_group)
     
+    if not os.path.exists(args.config):
+        # If the configuration file doesn't exist, create it.
+        for group in groups:
+            config.add_section(group[0])
+            for flag, _ in group[1:]:
+                config[group[0]][flag[-1][2:]] = ''
+
+        with open(args.config, 'w') as fp:
+            config.write(fp)
+                
+        _report_error(ErrorCode.config_file_not_found)
+    else:
+        try:
+            config.read(args.config)
+        except configparser.Error:
+            _report_error(ErrorCode.invalid_config_file)
+
+
+def main():
     args = _parse_args()
 
     if args.errcodes:
@@ -88,16 +125,7 @@ def main():
             print('{:d}: {}'.format(code, _errors[code]))
         _report_error(ErrorCode.ok)
 
-    if args.config:
-        config = configparser.ConfigParser()
-        
-        if not os.path.exists(args.config):
-            _report_error(ErrorCode.config_file_not_found)
-        else:
-            try:
-                config.read(args.config)
-            except configparser.Error as e:
-                _report_error(ErrorCode.invalid_config_file)
+    _load_config(args)
 
 
 if __name__ == '__main__':
