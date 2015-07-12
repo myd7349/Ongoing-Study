412b46f2615215a30badec43f0ad05463579f9a0
myd7349 <myd7349@gmail.com>
2015-07-12 20:58:23 +0800

    Add MoviesScreenshotsMgr.py

---
+++ python/MoviesScreenshotsMgr.py
@@ -0,0 +1,23 @@
+# coding: utf-8
+
+# 2015-07-12T20:46+08:00
+
+import datetime
+import imghdr
+import os
+import re
+from stat import *
+
+import fileutil
+
+
+def rename_scshot(oldname):
+    pass
+
+
+def dump_timeline():
+    pass
+
+
+
+
