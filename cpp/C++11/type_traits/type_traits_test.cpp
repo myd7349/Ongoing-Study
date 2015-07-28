d3dc87e0acb0ed16c9987feb9b2599dbfc6c50bb
myd7349 <myd7349@gmail.com>
2015-07-28 09:05:15 +0800

    DO NOT use decltype(*iter), keep using iterator_traits

--- cpp/C++11/type_traits/type_traits_test.cpp
+++ cpp/C++11/type_traits/type_traits_test.cpp
@@ -1,13 +1,7 @@
 // 2015-07-27T16:40+08:00
 #include <iostream>
-
-#ifndef CONFIG_USING_CXX11
-# define CONFIG_USING_CXX11 (1)
-#endif
-
-#if CONFIG_USING_CXX11
-# include <type_traits>
-#endif
+#include <type_traits>
+#include <vector>
 
 #include "../../common.h"
 #include "../../uniform_typename/uniform_typename.h"
@@ -17,17 +11,27 @@ int main()
     int arr[] = { 1, 2, 3 };
     int *first = std::begin(arr);
 
-#if CONFIG_USING_CXX11
     using ValueT = decltype(*first);
     using ValueT2 = std::remove_reference<ValueT>::type;
 
     std::cout << std::is_reference<ValueT>::value << std::endl; // true, why?
     std::cout << std::is_reference<ValueT2>::value << std::endl;
     std::cout << std::is_reference<std::iterator_traits<int *>::value_type>::value << std::endl;
-    //assert(std::is_same<int &, ValueT>::value);
-    //assert(std::is_same<int, ValueT2>::value);
+
+#if !defined(_MSC_VER) || _MSC_VER > 1800
+    assert(std::is_same<int &, ValueT>::value);
+    assert(std::is_same<int, ValueT2>::value);
 #endif
+
+    // 
+    std::vector<int> ivec(10, 1);
+    std::vector<bool> bvec(10, true);
+    std::cout << TYPE_NAME(decltype(*ivec.begin())) << std::endl;
+    std::cout << TYPE_NAME(decltype(*bvec.begin())) << std::endl;
 
     PAUSE();
     return 0;
 }
+
+// References:
+// [How to declare the value of an iterator through decltype](http://stackoverflow.com/questions/15560912/how-to-declare-the-value-of-an-iterator-through-decltype)
