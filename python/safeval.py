ae55e401dde60c70e7e13d073a6e47394339c9ae
myd7349 <myd7349@gmail.com>
2015-08-03 10:13:32 +0800

    Update safeval.py

--- python/safeval.py
+++ python/safeval.py
@@ -3,13 +3,38 @@
 
 # 2015-07-11T13:03+08:00
 
+import tokenize
+
+
 class UnsafeError(Exception): pass
 
 
-def safe_eval(expr, allow_doublescore=False):
+# It's not easy to make `eval` safe and useful at the same time.
+# When I say `useful` I mean that what I want is not another
+# `ast.literal_eval`. Instead, I want `safe_eval`  be able to do more.
+# For example, some builtin functions like `int`, 'float' are safe,
+# so I shouldn't hide them all from `safe_eval`.
+# eval(expr, {'__builtins__': {}}, {}), is safe, but useless.
+# Here, I will define some rules to make the call to `eval` more safer:
+#   1. Calling arbitrary functions without limitations is dangerous, so
+#      a white list is defined. Only those functions that in this list
+#      can be appeared in the input expression. This list, of course,
+#      is immutable.
+#      (Since, `copy.deepcopy` will never create a deep copy of a function
+#       so things like this won't bother us:
+#         allowed_list = (sin, cos)
+#         import copy, os
+#         sin = copy.deepcopy(os.system)
+#      )
+#   2. Only object of those builtin types are allowed in the expression.
+#      (So that things like below won't happen:
+#         class Foo:
+#             def __init__(self):
+#                 os.system('/bin/sh')
+#         safe_eval('Foo()')
+#      )
+def safe_eval(expr):
     """A relatively safe `eval`."""
-    if not allow_doublescore and '__' in expr:
-        raise UnsafeError('For security propose, double underscore is not allowed')
 
     return eval(expr, {'__builtins__': {}}, {})
 
