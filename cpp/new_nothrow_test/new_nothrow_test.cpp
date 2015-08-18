efede3753885e655f3713a83b4b23997e1b7688d
myd7349 <myd7349@gmail.com>
2015-08-18 15:05:41 +0800

    Update new_nothrow_test

--- cpp/new_nothrow_test/new_nothrow_test.cpp
+++ cpp/new_nothrow_test/new_nothrow_test.cpp
@@ -2,6 +2,13 @@
 #include <iostream>
 #include <new> // for std::nothrow and nothrow allocation
 
+// 2015-08-18T15:04+08:00
+// Effective C++ 3rd Edition/Item 49:
+// Using nothrow new guarantees only that operator new won't throw, not that an
+//  expression like "new (std::nothrow) Widget" will never yield an exception.
+//  In all likelihood, you will never have a need for nothrow new.
+
+
 int main()
 {
     if (int *x = new (std::nothrow) int(10)) {
@@ -12,3 +19,4 @@ int main()
     }
     return 0;
 }
+
