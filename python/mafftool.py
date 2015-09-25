fe4c4e2df6bd75b1b7346524a17aff6afb058c38
myd7349 <myd7349@gmail.com>
2015-09-25 15:34:38 +0800

    Add SQLAlchemy

---
+++ python/mafftool.py
@@ -0,0 +1,22 @@
+#! /usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+# 2015-09-25T14:38+08:00
+
+import xml
+import zipfile
+
+
+def create_zip():
+    pass
+
+
+def create_maff(url):
+    pass
+
+
+# References:
+# [Mozilla Archive Format](https://en.wikipedia.org/wiki/Mozilla_Archive_Format)
+# [MAFF File Format](http://maf.mozdev.org/maff-file-format.html)
+# [The MAFF specification](http://maf.mozdev.org/maff-specification.html)
+# [Total Commander:decMaffWLX plugin]
