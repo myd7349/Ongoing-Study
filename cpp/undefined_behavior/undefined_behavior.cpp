84b40c2647b4517557f98a23f0752989f782abe4
myd7349 <myd7349@gmail.com>
2016-01-13 10:00:07 +0800

    Deprecated.

--- cpp/undefined_behavior/undefined_behavior.cpp
+++ cpp/undefined_behavior/undefined_behavior.cpp
@@ -12,6 +12,15 @@
 //        char name[] = "Darla";
 //        char c = name[10];
 
+// 2015-08-09T23:02+08:00
+// 3. Refer to an uninitialized built-in object is undefined.
+// 4. The relative order of initialization of non-local static objects defined
+//    in different translation units is undefined.
+//    An example from Effective C++ 3E/Item 04
+//        class FileSystem {...
+
+// 2016-01-13T09:58+08:00
+// Deprecated. It's meaningless.
 
 int main()
 {
