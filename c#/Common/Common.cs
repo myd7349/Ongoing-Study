029b6303358302f1b0be17b20f6ffac680937661
myd7349 <myd7349@gmail.com>
2016-03-03 15:41:04 +0800

    Add Common Library

---
+++ c#/Common/Common.cs
@@ -0,0 +1,17 @@
+﻿// 2016-03-03T15:17+08:00
+
+using System;
+
+namespace Common
+{
+    public sealed class Common
+    {
+        public static void Pause(string prompt = "Press any key to quit!")
+        {
+            Console.Write(prompt);
+            Console.ReadKey();
+        }
+
+        public static Action PauseNul = () => Pause("");
+    }
+}
