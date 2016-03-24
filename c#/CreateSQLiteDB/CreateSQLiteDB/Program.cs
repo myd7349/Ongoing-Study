0bcc529e7eae8e74d15911e94b889816aadb9d9c
myd7349 <myd7349@gmail.com>
2016-03-24 16:59:34 +0800

    Add CreateSQLiteDB

---
+++ c#/CreateSQLiteDB/CreateSQLiteDB/Program.cs
@@ -0,0 +1,43 @@
+﻿// 2016-03-24T11:58+08:00
+using System;
+using System.IO;
+using System.Data.SQLite;
+
+using Common;
+using SQLiteHelper;
+
+// Create a sqlite3 database from an external sql script file.
+
+// TODO: Extended commandline options support.
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        if (args.Length < 2)
+        {
+            string usage = "Usage: {0} sql-script-file target-db-filename";
+            Console.WriteLine(usage, Util.GetProgName());
+            return;
+        }
+
+        string sqlScriptFile = args[0];
+        string targetDBFile = args[1];
+
+        string connectionStr = $"Data Source={targetDBFile};Version=3;";
+
+        SQLiteConnection.CreateFile(targetDBFile);
+        using (SQLiteConnection connection = new SQLiteConnection(connectionStr))
+        {
+            connection.Open();
+            connection.ExecuteScriptFile(sqlScriptFile);
+        }
+
+        Util.Pause();
+    }
+}
+
+// References:
+// [Create SQLite Database and table](http://stackoverflow.com/questions/15292880/create-sqlite-database-and-table)
+// [SQLite connection strings](http://www.connectionstrings.com/sqlite/)
+
---
+++ c#/CreateSQLiteDB/SQLiteHelper/SQLiteHelper.cs
@@ -0,0 +1,44 @@
+﻿// 2016-03-24T15:58+08:00
+using System.IO;
+using System.Data.SQLite; // Install-Package System.Data.SQLite
+using System.Diagnostics.Contracts;
+
+// In Python 3.x, class sqlite3.Connection contains a method named
+// 'executescript' which makes it very convenient to create a cursor
+// object from a piece of SQL script. I love the way it works, so
+// here we go!
+
+namespace SQLiteHelper
+{
+    public static class SQLiteConnectionHelper
+    {
+        public static SQLiteConnection ExecuteScript(
+            this SQLiteConnection connection,
+            string scriptSnippet)
+        {
+            Contract.Requires(connection.State == System.Data.ConnectionState.Open);
+            Contract.Requires(!string.IsNullOrEmpty(scriptSnippet));
+
+            using (SQLiteCommand command = connection.CreateCommand())
+            {
+                command.CommandText = scriptSnippet;
+                command.ExecuteNonQuery();
+            }
+
+            return connection;
+        }
+
+        public static SQLiteConnection ExecuteScriptFile(
+            this SQLiteConnection connection,
+            string scriptFilename)
+        {
+            Contract.Requires(!string.IsNullOrEmpty(scriptFilename));
+            Contract.Requires(Directory.Exists(scriptFilename));
+
+            return connection.ExecuteScript(File.ReadAllText(scriptFilename));
+        }
+    }
+}
+
+// References:
+// [create sqlite database with c# by using external ramin.sql file](http://stackoverflow.com/questions/13353476/create-sqlite-database-with-c-sharp-by-using-external-ramin-sql-file)
