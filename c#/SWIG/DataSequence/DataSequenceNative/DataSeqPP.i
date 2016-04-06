9a9e2a2490399819c974a2a8aeed4d85cbd1dc2a
myd7349 <myd7349@gmail.com>
2016-04-06 12:49:51 +0800

    SWIG test between C++ and C#

---
+++ c#/SWIG/DataSequence/DataSequenceNative/DataSeqPP.i
@@ -0,0 +1,8 @@
+/* 2016-04-06T09:55+08:00 */
+%module DataSequenceNative
+
+%{
+#include "../../../../cpp/DLL/DataSeqPP/DataSeqPP/DataSeqPP.h"  
+%}
+
+%include "../../../../cpp/DLL/DataSeqPP/DataSeqPP/DataSeqPP.h"
---
+++ c#/SWIG/DataSequence/Test/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+using Common;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        using (DataSequence ds = new DataSequence())
+        {
+            ds.push_back(3.14);
+            DumpDataSequence(ds);
+
+            ds.push_back(2.71);
+            ds.push_back(100);
+            DumpDataSequence(ds);
+
+            ds.pop_back();
+            DumpDataSequence(ds);
+
+            ds.pop_back();
+            ds.pop_back();
+            //ds.pop_back();
+            DumpDataSequence(ds);
+        }
+
+        Util.Pause();
+    }
+
+    static void DumpDataSequence(DataSequence ds)
+    {
+        if (ds == null)
+            return;
+
+        uint size = ds.size();
+        if (size == unchecked((uint)-1))
+        {
+            return;
+        }
+        else if (size == 0)
+        {
+            Console.WriteLine("[]");
+            return;
+        }
+
+        Console.Write("[{0}", ds.at(0));
+        for (uint i = 1; i < size; ++i)
+            Console.Write(", {0}", ds.at(i));
+        Console.WriteLine("]");
+    }
+}
+
