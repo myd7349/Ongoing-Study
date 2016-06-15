c97f80b15bebec5e57b5a4e0cb2d12e02c50ec0c
myd7349 <myd7349@gmail.com>
2016-06-15 11:19:37 +0800

    Add ArrayHelper.cs; Add CompareArrayTest

---
+++ c#/Common/ArrayHelper.cs
@@ -0,0 +1,33 @@
+﻿// 2016-06-15T11:12+08:00
+using System.Collections.Generic;
+
+
+namespace Common
+{
+    public static class ArrayHelper
+    {
+        public static bool EqualTo<T>(this T[] lhs, T[] rhs)
+        {
+            if (ReferenceEquals(lhs, rhs))
+                return true;
+
+            if (lhs == null || rhs == null)
+                return false;
+
+            if (lhs.Length != rhs.Length)
+                return false;
+
+            EqualityComparer<T> comparer = EqualityComparer<T>.Default;
+            for (int i = 0; i < lhs.Length; i++)
+            {
+                if (!comparer.Equals(lhs[i], rhs[i]))
+                    return false;
+            }
+
+            return true;
+        }
+    }
+}
+
+// References:
+// [Comparing arrays in C#](http://stackoverflow.com/questions/713341/comparing-arrays-in-c-sharp)
---
+++ c#/Console/CompareArrayTest/CompareArrayTest/Program.cs
@@ -0,0 +1,39 @@
+﻿// 2016-06-15T11:07+08:00
+using System;
+using System.Diagnostics;
+using System.IO;
+
+using Common;
+
+namespace CompareArrayTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var bytes = Properties.Resources.DeputyWave;
+
+            int dataLength = bytes.Length / sizeof(double);
+            double[] data1 = new double[dataLength];
+            double[] data2 = data1.Clone() as double[];
+
+            // 1.
+            Buffer.BlockCopy(bytes, 0, data1, 0, bytes.Length);
+
+            // 2.
+            using (var stream = new MemoryStream(bytes))
+            using (var reader = new BinaryReader(stream))
+            {
+                for (int i = 0; i < data2.Length; ++i)
+                    data2[i] = reader.ReadDouble();
+            }
+
+            Debug.Assert(data1.EqualTo(data2));
+
+            Util.Pause();
+        }
+    }
+}
+
+// References:
+// [How do I convert byte[] to stream in C#?](http://stackoverflow.com/questions/4736155/how-do-i-convert-byte-to-stream-in-c)
