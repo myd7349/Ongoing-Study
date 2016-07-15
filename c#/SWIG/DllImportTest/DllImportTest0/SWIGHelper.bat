5643881b4bb80512899292ad0a0f6e994ec262b9
myd7349 <myd7349@gmail.com>
2016-07-15 15:15:15 +0800

    Fix the DllNotFoundException

--- c#/SWIG/DllImportTest/DllImportTest0/SWIGHelper.bat
+++ c#/SWIG/DllImportTest/DllImportTest0/SWIGHelper.bat
@@ -9,7 +9,12 @@ cmd /c swig -help > nul
 if not errorlevel 0 goto SWIGNotFound
 
 echo Invoking SWIG...
-swig -c++ -csharp -namespace %1 -dllimport %1.Native -outdir "%~dp0%2" -outfile %3.cs "%4"
+:: Windows will not automatically append a .dll extension to library names that already have a period (.) 
+:: in their name, such as libgtk-win32-2.0-0.dll. If you try to use libgtk-win32-2.0-0 as the library name, 
+:: Windows won’t automatically append .dll, resulting in a DllNotFoundException. Consequently you should 
+:: either avoid periods in library names or always use the full filename (including the .dll extension) and
+:: rely on Mono’s <dllmap/> mechanism.
+swig -c++ -csharp -namespace %1 -dllimport %1.Native.dll -outdir "%~dp0%2" -outfile %3.cs "%4"
 goto Finish
 
 :SWIGNotFound
@@ -17,3 +22,6 @@ echo Command 'swig' not found.
 
 :Finish
 echo on
+
+:: References:
+:: http://www.mono-project.com/docs/advanced/pinvoke/
