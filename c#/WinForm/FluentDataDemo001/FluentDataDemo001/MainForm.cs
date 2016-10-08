bd8020426d3d62f803b9f58b23b5ab28608a4047
myd7349 <myd7349@gmail.com>
2016-10-08 17:12:09 +0800

    Finish data binding

--- c#/WinForm/FluentDataDemo001/FluentDataDemo001/MainForm.cs
+++ c#/WinForm/FluentDataDemo001/FluentDataDemo001/MainForm.cs
@@ -1,7 +1,10 @@
 ﻿// 2016-09-29T16:07+08:00
 using System;
+using System.Data;
 using System.Diagnostics;
 using System.IO;
+using System.Linq;
+using System.Xml.Linq;
 using System.Windows.Forms;
 
 using FluentData;
@@ -15,7 +18,13 @@ namespace FluentDataDemo001
             InitializeComponent();
         }
 
-        private bool createDatabase()
+        private IDbContext Context
+        {
+            get { return new DbContext().ConnectionString(GetConnectionString(), new SqliteProvider()); }
+        }
+
+#if false
+        private bool CreateDatabase()
         {
             var appPath = Application.StartupPath;
             var createCmd = Path.Combine(appPath, "CreateSQLiteDB.exe");
@@ -39,11 +48,52 @@ namespace FluentDataDemo001
 
             return true;
         }
+#else
+        private bool CreateDatabase()
+        {
+            Debug.Assert(Context != null);
+            if (Context != null)
+            {
+                var appPath = Application.StartupPath;
+                var scriptPath = Path.Combine(appPath, "schema.sql");
+
+                try
+                {
+                    Context.Sql(File.ReadAllText(scriptPath)).Execute();
+                    return true;
+                }
+                catch (Exception e)
+                {
+                    MessageBox.Show(e.ToString(), "Failed to create database file:",
+                        MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    return false;
+                }
+            }
+
+            return false;
+        }
+#endif
+
+        private string GetConnectionString()
+        {
+            var document = XDocument.Load(AppDomain.CurrentDomain.SetupInformation.ConfigurationFile);
+            var addElements = document.Element("configuration").Element("connectionStrings").Elements("add");
+            var addElement = addElements.Single(x => x.Attribute("name").Value == "FluentDataDemo001");
+            return addElement.Attribute("connectionString").Value;
+        }
 
         private void MainForm_Load(object sender, EventArgs e)
         {
-            if (!createDatabase())
+            var dbFile = GetConnectionString().Split(';').Single(x => x.StartsWith("Data Source")).Split('=').Last();
+            Debug.Assert(dbFile != null);
+
+            if (!File.Exists(dbFile) && !CreateDatabase())
                 Close();
+
+            var dataTable = Context.Sql("select * from Students").QuerySingle<DataTable>();
+
+            dataGridView.AutoGenerateColumns = true;
+            dataGridView.DataSource = dataTable;
         }
     }
 }
