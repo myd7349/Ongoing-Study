0e8dc97313f87ed1d151cf5658cd25ca52907d4a
myd7349 <myd7349@gmail.com>
2016-11-25 18:31:31 +0800

    Add bitsutils.hpp

---
+++ cpp/bitsutils.hpp
@@ -0,0 +1,31 @@
+// 2016-11-24T14:05+08:00
+#ifndef BITSUTILS_HPP_
+#define BITSUTILS_HPP_
+
+// TODO: Concepts
+
+template <typename T>
+inline T SetBits(T value, T bits)
+{
+    return value | bits;
+}
+
+template <typename T>
+inline T ClearBits(T value, T bits)
+{
+    return value & (~bits);
+}
+
+template <typename T>
+inline T TriggleBits(T value, T bits, bool set)
+{
+    return set ? SetBits(value, bits) : ClearBits(value, bits);
+}
+
+template <typename T>
+inline bool IsBitsSet(T value, T bits)
+{
+    return (value & bits) != 0;
+}
+
+#endif // BITSUTILS_HPP_
