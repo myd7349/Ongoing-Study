d105f9854df6d1463470ee90018efd435bf4ef7a
myd7349 <myd7349@gmail.com>
2016-11-28 18:55:05 +0800

    Make it behaves like QString::split

--- cpp/split.hpp
+++ cpp/split.hpp
@@ -3,87 +3,81 @@
 #define SPLIT_HPP_
 
 #include <cassert>
-#include <sstream>
+//#include <sstream>
 #include <string>
-#include <vector>
 
 
-#define string_list std::vector<std::basic_string<CharT>>
-
-
-// Behaves like strtok
-template <typename CharT>
-string_list split_by_tokens(const std::basic_string<CharT> &s, const std::basic_string<CharT> &tokens)
+// split("a,b,,c,", ",", true) -> <a><b><><c><>
+// split("a,b,,c,", ",", false) -> <a><b><c>
+template <typename ResultList, typename CharT>
+ResultList split(const std::basic_string<CharT> &s,
+    const std::basic_string<CharT> &delimiter, bool keepEmptyParts = true)
 {
-    string_list slist;
+    ResultList slist;
+
+    if (delimiter.empty())
+        return slist;
 
     typename std::basic_string<CharT>::size_type start = 0;
     typename std::basic_string<CharT>::size_type pos;
 
-    while ((pos = s.find_first_of(tokens, start)) != s.npos) {
-        slist.push_back(s.substr(start, pos - start));
-        start = pos + 1;
+    std::basic_string<CharT> part;
+
+    if (delimiter.length() == 1) {
+        CharT ch = delimiter[0];
+
+        // Hope that:
+        //   find(std::basic_string<CharT>, CharT ch)
+        // will be faster than:
+        //   find(std::basic_string<CharT>, std::basic_string<CharT>)
+        while ((pos = s.find(ch, start)) != s.npos) { // Use strchr/wcschr instead?
+            part = s.substr(start, pos - start);
+
+            if (!part.empty() || keepEmptyParts)
+                slist.push_back(part);
+
+            start = pos + delimiter.length();
+        }
+    } else {
+        while ((pos = s.find(delimiter, start)) != s.npos) { // Use strstr/wcsstr instead?
+            part = s.substr(start, pos - start);
+
+            if (!part.empty() || keepEmptyParts)
+                slist.push_back(part);
+
+            start = pos + delimiter.length();
+        }
     }
 
-    if (start < s.length())
+    if (start != s.length() || keepEmptyParts)
         slist.push_back(s.substr(start));
 
     return slist;
 }
 
-template <typename CharT>
-inline string_list split_by_tokens(const std::basic_string<CharT> &s, const CharT *tokens)
-{
-    assert(tokens != nullptr);
-    return split_by_tokens(s, std::basic_string<CharT>(tokens));
-}
 
-template <typename CharT>
-inline string_list split_by_tokens(const CharT *s, const CharT *tokens)
-{
-    assert(s != nullptr);
-    return split_by_tokens(std::basic_string<CharT>(s), tokens);
-}
-
-// Use strstr/wcsstr instead?
-template <typename CharT>
-string_list split(const std::basic_string<CharT> &s, const std::basic_string<CharT> &delimiter)
-{
-    string_list slist;
-
-    typename std::basic_string<CharT>::size_type start = 0;
-    typename std::basic_string<CharT>::size_type pos;
-
-    while ((pos = s.find(delimiter, start)) != s.npos) {
-        slist.push_back(s.substr(start, pos - start));
-        start = pos + delimiter.length();
-    }
-
-    if (start < s.length())
-        slist.push_back(s.substr(start));
-
-    return slist;
-}
-
-template <typename CharT>
-inline string_list split(const std::basic_string<CharT> &s, const CharT *delimiter)
+template <typename ResultList, typename CharT>
+inline ResultList split(const std::basic_string<CharT> &s,
+    const CharT *delimiter, bool keepEmptyParts = true)
 {
     assert(delimiter != nullptr);
-    return split(s, std::basic_string<CharT>(delimiter));
+    return split<ResultList>(s, std::basic_string<CharT>(delimiter), keepEmptyParts);
 }
 
-template <typename CharT>
-inline string_list split(const CharT *s, const CharT *delimiter)
+
+template <typename ResultList, typename CharT>
+inline ResultList split(const CharT *s, const CharT *delimiter, bool keepEmptyParts = true)
 {
     assert(s != nullptr);
-    return split(std::basic_string<CharT>(s), delimiter);
+    return split<ResultList>(std::basic_string<CharT>(s), delimiter, keepEmptyParts);
 }
 
-// Use strchr/wcschr instead?
-template <typename CharT>
-string_list split(const std::basic_string<CharT> &s, CharT ch)
+
+template <typename ResultList, typename CharT>
+inline ResultList split(const std::basic_string<CharT> &s, CharT ch, bool keepEmptyParts = true)
 {
-    string_list slist;
+#if 0
+    ResultList slist;
 
     std::basic_istringstream<CharT> ss(s);
     std::basic_string<CharT> item;
@@ -91,17 +85,73 @@ string_list split(const std::basic_string<CharT> &s, CharT ch)
         slist.push_back(item);
 
     return slist;
+#endif
+
+    return split<ResultList>(s, std::basic_string<CharT>(1, ch), keepEmptyParts);
 }
 
-template <typename CharT>
-inline string_list split(const CharT *s, CharT ch)
+
+template <typename ResultList, typename CharT>
+inline ResultList split(const CharT *s, CharT ch, bool keepEmptyParts = true)
 {
-    return split(std::basic_string<CharT>(s), ch);
+    return split<ResultList>(std::basic_string<CharT>(s), ch, keepEmptyParts);
 }
 
-#undef string_list
+
+// split_by_tokens("a,b-,c,", ",-", true) -> <a><b><><c><>
+// split_by_tokens("a,b-,c,", ",-", false) -> <a><b><c>
+template <typename ResultList, typename CharT>
+ResultList split_by_tokens(const std::basic_string<CharT> &s,
+    const std::basic_string<CharT> &tokens, bool keepEmptyParts = true)
+{
+    if (tokens.length() == 1)
+        return split<ResultList>(s, tokens, keepEmptyParts);
+
+    ResultList slist;
+
+    if (tokens.empty())
+        return slist;
+
+    typename std::basic_string<CharT>::size_type start = 0;
+    typename std::basic_string<CharT>::size_type pos;
+
+    std::basic_string<CharT> part;
+
+    while ((pos = s.find_first_of(tokens, start)) != s.npos) { // strtok
+        part = s.substr(start, pos - start);
+
+        if (!part.empty() || keepEmptyParts)
+            slist.push_back(part);
+
+        start = pos + 1;
+    }
+
+    if (start != s.length() || keepEmptyParts)
+        slist.push_back(s.substr(start));
+
+    return slist;
+}
+
+
+template <typename ResultList, typename CharT>
+inline ResultList split_by_tokens(const std::basic_string<CharT> &s,
+    const CharT *tokens, bool keepEmptyParts = true)
+{
+    assert(tokens != nullptr);
+    return split_by_tokens<ResultList>(s, std::basic_string<CharT>(tokens), keepEmptyParts);
+}
+
+
+template <typename ResultList, typename CharT>
+inline ResultList split_by_tokens(const CharT *s, const CharT *tokens, bool keepEmptyParts = true)
+{
+    assert(s != nullptr);
+    return split_by_tokens<ResultList>(std::basic_string<CharT>(s), tokens, keepEmptyParts);
+}
+
 
 #endif // SPLIT_HPP_
 
 // References:
 // [Split a string in C++?](http://stackoverflow.com/questions/236129/split-a-string-in-c)
+// Qt:QString::split
