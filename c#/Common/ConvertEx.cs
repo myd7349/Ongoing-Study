373ee096922f6d1b4b644d91cb7c9a5225304a97
myd7349 <myd7349@gmail.com>
2017-02-24 14:54:50 +0800

    Added ConvertEx.cs.

---
+++ c#/Common/ConvertEx.cs
@@ -0,0 +1,16 @@
+﻿// 2017-02-23T20:30+08:00
+using System;
+
+// System.Convert.ToDouble("25 mm/s") -> Bang!
+// Common.ConvertEx.ToDouble("25 mm/s") -> 25.0
+
+namespace Common
+{
+    public static class ConvertEx
+    {
+        public static double ToDouble(string value)
+        {
+            return MSVCRTHelper.strtod(value, IntPtr.Zero);
+        }
+    }
+}
---
+++ c#/Common/MSVCRTHelper.cs
@@ -0,0 +1,12 @@
+﻿// 2017-02-23T20:30+08:00
+using System;
+using System.Runtime.InteropServices;
+
+namespace Common
+{
+    public static class MSVCRTHelper
+    {
+        [DllImport("msvcrt.dll", CallingConvention = CallingConvention.Cdecl, CharSet = CharSet.Ansi)]
+        public static extern double strtod(string value, IntPtr endptr);
+    }
+}
