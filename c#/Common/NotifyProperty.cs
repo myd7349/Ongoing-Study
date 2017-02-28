6f18f95099c98a329209585b3189e0f1dca9c098
myd7349 <myd7349@gmail.com>
2017-02-28 13:39:11 +0800

    Added NotifyProperty<T>

---
+++ c#/Common/NotifyProperty.cs
@@ -0,0 +1,59 @@
+﻿// 2017-02-26T10:18+08:00
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+
+namespace Common
+{
+    public class NotifyProperty<T> : INotifyPropertyChanged
+    {
+        public NotifyProperty(T value, string name = "")
+        {
+            propertyValue = value;
+            propertyName = name;
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public T PropertyValue
+        {
+            get
+            {
+                return propertyValue;
+            }
+
+            set
+            {
+                //error CS0019: Operator '==' cannot be applied to operands of type 'T' and 'T'
+                //if ((T)value == propertyValue) return;
+
+                if (EqualityComparer<T>.Default.Equals(value, propertyValue))
+                    return;
+
+                propertyValue = value;
+                OnPropertyChanged(propertyName);
+            }
+        }
+
+        public static implicit operator T(NotifyProperty<T> property)
+        {
+            Debug.Assert(property != null);
+            if (property == null)
+                return default(T);
+
+            return property.PropertyValue;
+        }
+
+        private void OnPropertyChanged(string propertyName)
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        private T propertyValue;
+        private string propertyName;
+    }
+}
+
+// References:
+// [How to solve Operator '!=' cannot be applied to operands of type 'T' and 'T'](http://stackoverflow.com/questions/8982645/how-to-solve-operator-cannot-be-applied-to-operands-of-type-t-and-t)
---
+++ c#/Console/NotifyPropertyTest/Program.cs
@@ -0,0 +1,57 @@
+﻿// 2017-02-28T13:27:00+08:00
+using System;
+using System.ComponentModel;
+
+using Common;
+
+namespace NotifyPropertyTest
+{
+    class Person
+    {
+        public Person()
+        {
+            name.PropertyChanged += OnPropertyChanged;
+            age.PropertyChanged += OnPropertyChanged;
+        }
+
+        public string Name
+        {
+            get { return name; }
+            set { name.PropertyValue = value; }
+        }
+
+        public int Age
+        {
+            get { return age; }
+            set { age.PropertyValue = value; }
+        }
+
+        private void OnPropertyChanged(object sender, PropertyChangedEventArgs e)
+        {
+            switch (e.PropertyName)
+            {
+                case "Name":
+                    Console.WriteLine("Name changed. New name is: {0}", Name);
+                    break;
+                case "Age":
+                    Console.WriteLine("Age changed. New age is: {0}", Age);
+                    break;
+            }
+        }
+
+        private NotifyProperty<string> name = new NotifyProperty<string>("Tom", "Name");
+        private NotifyProperty<int> age = new NotifyProperty<int>(10, "Age");
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var p = new Person();
+            p.Name = "Jim";
+            p.Age = 12;
+
+            Util.Pause();
+        }
+    }
+}
