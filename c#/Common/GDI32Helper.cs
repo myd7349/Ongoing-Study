40b1cb91b362ac60a42041b8fae2da2e1084ac9b
myd7349 <myd7349@gmail.com>
2017-04-12 16:04:41 +0800

    Refactoring.

---
+++ c#/Common/GDI32Helper.cs
@@ -0,0 +1,64 @@
+﻿// 2016-05-30T13:38+08:00
+using System;
+using System.Drawing;
+using System.Runtime.InteropServices;
+
+
+namespace Common.Auxiliary.Drawing
+{
+    using COLORREF = UInt32;
+
+    public static class Gdi32Helper
+    {
+        // Thanks to PInvoke.net for the signatures of GetPixel/SetPixel.
+        [DllImport("gdi32.dll")]
+        public static extern COLORREF GetPixel(IntPtr hdc, int nXPos, int nYPos);
+
+        [DllImport("gdi32.dll")]
+        public static extern COLORREF SetPixel(IntPtr hdc, int X, int Y, COLORREF crColor);
+
+        // MSDN:
+        // COLORREF: 0x00bbggrr
+        // Color.ToArgb: 0xAARRGGBB
+        public static COLORREF RGB(byte r, byte g, byte b)
+        {
+            COLORREF rgb = r;
+            rgb |= (UInt16)(g << 8);
+            rgb |= (UInt32)(b << 16);
+            return rgb; // WinGDI.h:1595
+        }
+
+        public static byte GetRValue(COLORREF rgb)
+        {
+            return (byte)(rgb & 0xff); // WinGDI.h:1605
+        }
+
+        public static byte GetGValue(COLORREF rgb)
+        {
+            return (byte)(((UInt16)rgb) >> 8); // WinGDI.h:1606
+        }
+
+        public static byte GetBValue(COLORREF rgb)
+        {
+            return (byte)((rgb >> 16) & 0xff); // WinGDI.h:1607 
+        }
+    }
+
+    public static class ColorExtension
+    {
+        //public static Color FromCOLORREF(this Color color, COLORREF clr) [2]
+        public static Color FromCOLORREF(COLORREF clr)
+        {
+            return ColorTranslator.FromWin32((int)clr);
+        }
+
+        public static COLORREF ToCOLORREF(this Color color)
+        {
+            return (COLORREF)ColorTranslator.ToWin32(color);
+        }
+    }
+}
+
+// References:
+// [1](http://www.pinvoke.net/)
+// [2](http://stackoverflow.com/questions/866921/static-extension-methods)
--- c#/Common/GraphicsExtension.cs
+++ c#/Common/GraphicsExtension.cs
@@ -1,68 +1,14 @@
-﻿// 2016-05-30T13:38+08:00
+﻿// 2017-04-07T12:17+08:00
 using System;
 using System.Diagnostics;
 using System.Drawing;
 using System.Linq;
-using System.Runtime.InteropServices;
 using System.Text.RegularExpressions;
 
 
 namespace Common.Auxiliary.Drawing
 {
     using COLORREF = UInt32;
-
-    public static class Gdi32Helper
-    {
-        // Thanks to PInvoke.net for the signatures of GetPixel/SetPixel.
-        [DllImport("gdi32.dll")]
-        public static extern COLORREF GetPixel(IntPtr hdc, int nXPos, int nYPos);
-
-        [DllImport("gdi32.dll")]
-        public static extern COLORREF SetPixel(IntPtr hdc, int X, int Y, COLORREF crColor);
-
-        // MSDN:
-        // COLORREF: 0x00bbggrr
-        // Color.ToArgb: 0xAARRGGBB
-        public static COLORREF RGB(byte r, byte g, byte b)
-        {
-            COLORREF rgb = r;
-            rgb |= (UInt16)(g << 8);
-            rgb |= (UInt32)(b << 16);
-            return rgb; // WinGDI.h:1595
-        }
-
-        public static byte GetRValue(COLORREF rgb)
-        {
-            return (byte)(rgb & 0xff); // WinGDI.h:1605
-        }
-
-        public static byte GetGValue(COLORREF rgb)
-        {
-            return (byte)(((UInt16)rgb) >> 8); // WinGDI.h:1606
-        }
-
-        public static byte GetBValue(COLORREF rgb)
-        {
-            return (byte)((rgb >> 16) & 0xff); // WinGDI.h:1607 
-        }
-    }
-
-    public static class ColorExtension
-    {
-        //public static Color FromCOLORREF(this Color color, COLORREF clr) [2]
-        public static Color FromCOLORREF(COLORREF clr)
-        {
-            return Color.FromArgb(
-                Gdi32Helper.GetRValue(clr), 
-                Gdi32Helper.GetGValue(clr),
-                Gdi32Helper.GetBValue(clr));
-        }
-
-        public static COLORREF ToCOLORREF(this Color color)
-        {
-            return Gdi32Helper.RGB(color.R, color.G, color.B);
-        }
-    }
 
     public static class GraphicsExtension
     {
@@ -80,6 +26,22 @@ namespace Common.Auxiliary.Drawing
             COLORREF rgb = Gdi32Helper.SetPixel(hdc, x, y, color.ToCOLORREF());
             g.ReleaseHdc(hdc);
             return ColorExtension.FromCOLORREF(rgb);
+        }
+
+        public static void SetPixel(this Graphics graphics, float x, float y, Brush brush)
+        {
+            Debug.Assert(graphics != null);
+            Debug.Assert(brush != null);
+
+            graphics.FillRectangle(brush, x, y, 1.0F, 1.0F); // [1]
+        }
+
+        public static void DrawRectangle(this Graphics graphics, Pen pen, RectangleF rect)
+        {
+            Debug.Assert(graphics != null);
+            Debug.Assert(pen != null);
+
+            graphics.DrawRectangle(pen, rect.X, rect.Y, rect.Width, rect.Height);
         }
 
         private static bool DrawSingleLineStringAutoWrap(Graphics g, Font font, Brush brush,
@@ -162,9 +124,7 @@ namespace Common.Auxiliary.Drawing
     }
 }
 
-// References:
-// [1](http://www.pinvoke.net/)
-// [2](http://stackoverflow.com/questions/866921/static-extension-methods)
+// [1](http://stackoverflow.com/questions/761003/draw-a-single-pixel-on-windows-forms)
 // [3](http://stackoverflow.com/questions/2923210/conditional-compilation-and-framework-targets)
 // [4](http://stackoverflow.com/questions/1508203/best-way-to-split-string-into-lines)
 // [5](http://stackoverflow.com/questions/25100324/array-slice-to-create-new-array)
