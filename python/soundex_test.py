8061f898ab14de6ee3426c981b050c085e340242
myd7349 <myd7349@gmail.com>
2017-09-11 09:37:04 +0800

    Update

--- python/soundex_test.py
+++ python/soundex_test.py
@@ -16,7 +16,7 @@ BEGIN TRANSACTION;
 -- Table: users
 CREATE TABLE users (
     id   INTEGER      NOT NULL
-                       PRIMARY KEY AUTOINCREMENT,
+                      PRIMARY KEY AUTOINCREMENT,
     name VARCHAR (30) NOT NULL
 );
 
@@ -29,6 +29,9 @@ PRAGMA foreign_keys = on;
 '''
 
 with contextlib.closing(sqlite3.connect(':memory:')) as connection:
+    # It seems that Python3's builtin SQLite3 module doesn't support
+    # the `SOUNDEX` function(May be the `SQLITE_SOUNDEX` option was
+    # not enabled when the module was compiled).
     connection.create_function('soundex', 1, jellyfish.soundex)
     
     with contextlib.closing(connection.executescript(sql)) as cursor:
@@ -36,3 +39,7 @@ with contextlib.closing(sqlite3.connect(':memory:')) as connection:
         print(cursor.fetchall())
 
 
+# References:
+# https://www.zhihu.com/question/65132459
+# [Using Fuzzy Matching to Search by Sound with Python](https://doughellmann.com/blog/2012/03/03/using-fuzzy-matching-to-search-by-sound-with-python/)
+# [How do I do a fuzzy match of company names in MYSQL with PHP for auto-complete?](https://stackoverflow.com/questions/369755/how-do-i-do-a-fuzzy-match-of-company-names-in-mysql-with-php-for-auto-complete)
