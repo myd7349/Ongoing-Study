0ce627a005297ebb4336515e23a38f60e38c1afe
myd7349 <myd7349@gmail.com>
2017-12-27 17:35:22 +0800

    Add TCPTest

---
+++ c#/Console/TCPTest/TCPClient/Program.cs
@@ -0,0 +1,43 @@
+﻿// 2017-12-27T12:13+08:00
+using System;
+using System.Net.Sockets;
+using System.Text;
+
+using Common;
+
+namespace TCPClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var clientSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp))
+            {
+                try
+                {
+                    clientSocket.Connect("localhost", 12001);
+
+                    const int BufferSize = 1024;
+                    var buffer = new byte[BufferSize];
+
+                    Console.Write("Input a sentence: ");
+                    var sentence = Console.ReadLine().TrimEnd('\r', '\n');
+                    clientSocket.Send(Encoding.UTF8.GetBytes(sentence));
+
+                    var receivedBytes = clientSocket.Receive(buffer);
+                    Console.WriteLine("Received from server: {0}", Encoding.UTF8.GetString(buffer, 0, receivedBytes));
+                }
+                catch (SocketException ex)
+                {
+                    Console.WriteLine(ex);
+                }
+            }
+
+            Util.Pause();
+        }
+    }
+}
+
+
+// References:
+// Computer Networking: A Top-Down Approach 5th Edition, 2.7
---
+++ c#/Console/TCPTest/TCPServer/Program.cs
@@ -0,0 +1,40 @@
+﻿// 2017-12-27T12:23+08:00
+using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+
+namespace TCPServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var serverSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp))
+            {
+                serverSocket.Bind(new IPEndPoint(IPAddress.Any, 12001));
+                serverSocket.Listen(1);
+
+                const int BufferSize = 1024;
+                var buffer = new byte[BufferSize];
+
+                while (true)
+                {
+                    var connectionSocket = serverSocket.Accept();
+                    var receivedBytes = connectionSocket.Receive(buffer);
+
+                    var sentence = Encoding.UTF8.GetString(buffer, 0, receivedBytes);
+                    Console.WriteLine("Received [{0}] from [{1}]", sentence, connectionSocket.RemoteEndPoint);
+
+                    connectionSocket.Send(Encoding.UTF8.GetBytes(sentence.ToUpper()));
+                    connectionSocket.Close();
+                }
+            }
+        }
+    }
+}
+
+
+// References:
+// Computer Networking: A Top-Down Approach 5th Edition, 2.7
