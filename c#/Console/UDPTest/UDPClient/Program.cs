c6ad2b5468cd5eaae96f6568a9909780a6823178
myd7349 <myd7349@gmail.com>
2017-12-28 15:39:28 +0800

    Not work.

---
+++ c#/Console/UDPTest/UDPClient/Program.cs
@@ -0,0 +1,42 @@
+﻿// 2017-12-28T15:01+08:00
+using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+using Common;
+
+
+namespace UDPClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var clientSocket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp))
+            {
+                Console.Write("Please input a sentence: ");
+                var sentence = Console.ReadLine();
+
+                var localHost = Dns.GetHostEntry("localhost");
+                var serverHost = new IPEndPoint(localHost.AddressList[1].MapToIPv4(), 12000);
+                clientSocket.SendTo(Encoding.UTF8.GetBytes(sentence), serverHost);
+
+                const int BufferLength = 1024;
+                var buffer = new byte[BufferLength];
+
+                try
+                {
+                    var byteCount = clientSocket.Receive(buffer);
+                    Console.WriteLine("Received from server: {0}", Encoding.UTF8.GetString(buffer, 0, byteCount));
+                }
+                catch (SocketException ex)
+                {
+                    Console.WriteLine(ex);
+                }
+            }
+
+            Util.Pause();
+        }
+    }
+}
---
+++ c#/Console/UDPTest/UDPServer/Program.cs
@@ -0,0 +1,33 @@
+﻿// 2017-12-28T15:01+08:00
+using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+
+namespace UDPServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var serverSocket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp))
+            {
+                serverSocket.Bind(new IPEndPoint(IPAddress.Any, 12000));
+
+                const int BufferLength = 1024;
+                var buffer = new byte[BufferLength];
+
+                while (true)
+                {
+                    var byteCount = serverSocket.Receive(buffer);
+                    var sentence = Encoding.UTF8.GetString(buffer, 0, byteCount);
+
+                    Console.WriteLine("Received [{0}] from [{1}].", sentence, serverSocket.RemoteEndPoint);
+
+                    serverSocket.SendTo(Encoding.UTF8.GetBytes(sentence.ToUpper()), serverSocket.RemoteEndPoint);
+                }
+            }
+        }
+    }
+}
