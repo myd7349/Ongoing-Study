cd699487e5f6590ecb7c5e795ae80cb16ae04d29
myd7349 <myd7349@gmail.com>
2017-12-29 13:27:27 +0800

    Fix

--- algorithm/math/sqrt/sqrt.c
+++ algorithm/math/sqrt/sqrt.c
@@ -3,11 +3,14 @@
 #include <math.h>
 
 
-// Newton¡¯s method to compute square roots.
+// Newton's method to compute square roots.
 double fsqrt(double x)
 {
     double epilson = 1e-15;
     double sr = x;
+
+    if (x == 0.0)
+        return 0.0;
 
     if (x < 0.0)
         return NAN;
@@ -23,3 +26,4 @@ double fsqrt(double x)
 // https://mitpress.mit.edu/sicp/chapter1/node9.html
 // https://math.mit.edu/~stevenj/18.335/newton-sqrt.pdf
 // Algorithms, 4th Edition, by Robert Sedgewick and Kevin Wayne
+// https://stackoverflow.com/questions/4745311/c-division-by-0
--- algorithm/math/sqrt/sqrt_test.c
+++ algorithm/math/sqrt/sqrt_test.c
@@ -1,3 +1,4 @@
+#include <math.h>
 #include <stdio.h>
 
 #include "../../algutils.h"
@@ -17,6 +18,9 @@ int main()
     TEST(4.0);
     TEST(100.0);
     TEST(-1.0);
+    TEST(NAN);
+    TEST(INFINITY);
+    TEST(-INFINITY);
 
     return 0;
 }
