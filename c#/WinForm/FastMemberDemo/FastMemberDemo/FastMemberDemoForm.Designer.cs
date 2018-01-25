a4dafa7acd1a9f87c57f41873f0cf2c2f036bd4b
myd7349 <myd7349@gmail.com>
2018-01-25 13:18:18 +0800

    Add FastMemberDemo

---
+++ c#/WinForm/FastMemberDemo/FastMemberDemo/FastMemberDemoForm.Designer.cs
@@ -0,0 +1,63 @@
+﻿namespace FastMemberDemo
+{
+    partial class FastMemberDemoForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(13, 13);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 23;
+            this.dataGridView1.Size = new System.Drawing.Size(259, 236);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // FastMemberDemoForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "FastMemberDemoForm";
+            this.Text = "FastMember Demo";
+            this.Load += new System.EventHandler(this.FastMemberDemoForm_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+    }
+}
+
---
+++ c#/WinForm/FastMemberDemo/FastMemberDemo/FastMemberDemoForm.cs
@@ -0,0 +1,45 @@
+﻿// 2018-01-25T13:16+08:00
+
+namespace FastMemberDemo
+{
+    using System;
+    using System.Data;
+    using System.Windows.Forms;
+
+    using FastMember;
+
+
+    public struct Student
+    {
+        public string Name;
+        public int Age;
+        public string Address;
+    }
+
+    public partial class FastMemberDemoForm : Form
+    {
+        public FastMemberDemoForm()
+        {
+            InitializeComponent();
+        }
+
+        private void FastMemberDemoForm_Load(object sender, EventArgs e)
+        {
+            var students = new Student[]
+            {
+                new Student() { Name = "Tom", Age = 12, Address = "NYC" },
+                new Student() { Name = "Jerry", Age = 8, Address = "NYC" },
+            };
+
+            var dataTable = new DataTable();
+            using (var reader = ObjectReader.Create(students, "Name", "Age", "Address"))
+            {
+                dataTable.Load(reader);
+            }
+
+            dataGridView1.DataSource = dataTable;
+        }
+    }
+}
+
+// https://stackoverflow.com/questions/564366/convert-generic-list-enumerable-to-datatable
