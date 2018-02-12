16192d0ca4bc9f1e0a36eb2a78fe3401821ebcae
myd7349 <myd7349@gmail.com>
2018-02-12 09:51:41 +0800

    Add local_static_variable_initialization

---
+++ cpp/C++11/local_static_variable_initialization/main.cpp
@@ -0,0 +1,45 @@
+// 2018-02-12T09:38+08:00
+#include <iostream>
+#include <thread>
+
+
+#define ENTER_FUNC() do { std::cout << __LINE__ << " Enter " << __func__ << std::endl; } while (0)
+#define LEAVE_FUNC() do { std::cout << __LINE__ << " Leave " << __func__ << std::endl; } while (0)
+
+
+struct MyClass
+{
+    MyClass()
+    {
+        std::cout << "Constructing MyClass" << std::endl;
+    }
+};
+
+
+const MyClass &CreateMyClass()
+{
+    ENTER_FUNC();
+    static MyClass myClass;
+    LEAVE_FUNC();
+    return myClass;
+}
+
+
+int main()
+{
+    ENTER_FUNC();
+
+    std::thread thrd1(CreateMyClass);
+    std::thread thrd2(CreateMyClass);
+
+    thrd1.join();
+    thrd2.join();
+
+    LEAVE_FUNC();
+
+    return 0;
+}
+
+// References:
+// https://www.zhihu.com/question/267013757
+// https://stackoverflow.com/questions/1661529/is-meyers-implementation-of-the-singleton-pattern-thread-safe
