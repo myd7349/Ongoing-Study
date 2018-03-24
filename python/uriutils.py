e89e9ac5b3707cd284d69f6c6875b498254d4d5f
myd7349 <myd7349@gmail.com>
2018-03-24 19:19:00 +0800

    UA

--- python/uriutils.py
+++ python/uriutils.py
@@ -3,9 +3,25 @@
 
 # 2018-01-16T10:12+08:00
 
+import urllib.request
+
 import pyquery  # pip install pyquery
 
-from FetchPythonTutorialsURLs import fetchPage as fetch_page
+
+# UA definition shamelessly stolen from https://github.com/kennethreitz/requests-html/blob/master/requests_html.py
+DEFAULT_USER_AGENT = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/603.3.8 (KHTML, like Gecko) Version/10.1.2 Safari/603.3.8'
+
+
+def fetch_page(url, encoding='utf-8'):
+    request = urllib.request.Request(url, headers={'User-Agent': DEFAULT_USER_AGENT})
+    response = urllib.request.urlopen(request)
+    
+    if encoding:
+        charset = response.headers.get_content_charset(failobj = encoding)
+        for line in response:
+            yield line.decode(charset)
+    else:
+        yield from response
 
 
 def fetch_page_contents(url, encoding='utf-8'):
@@ -20,3 +36,4 @@ def iurl(page_contents):
 # References:
 # https://stackoverflow.com/questions/9535732/how-to-extract-all-the-urls-from-a-website
 # http://pyquery.readthedocs.io/en/latest/
+# https://stackoverflow.com/questions/802134/changing-user-agent-on-urllib2-urlopen
