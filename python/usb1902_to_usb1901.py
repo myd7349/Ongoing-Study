0cfd42496be7b6fc6dd47c431ac445460fb2238b
myd7349 <myd7349@gmail.com>
2018-04-04 13:47:48 +0800

    Fix

--- python/usb1902_to_usb1901.py
+++ python/usb1902_to_usb1901.py
@@ -17,10 +17,12 @@ for code_file in code_files:
     with open(code_file, 'rb') as fp:
         with open(new_code_file, 'wb') as fp2:
             for line in fp:
-                match_res = re.match(b'(.*?[^A-Za-z0-9])(USB_1902)([^A-Za-z0-9].*?)', line)
-                if match_res:
-                    fp2.write(match_res.group(1)+b'USB_1901'+match_res.group(3))
-                    print(code_file, line)
+                if b'USB_1902' in line:
+                    #match_res = re.match(b'(.*?[^A-Za-z0-9])(?<!case )USB_1902([^A-Za-z0-9].*)', line, re.DOTALL)
+                    #new_line = match_res.group(1) + b'USB_1901' + match_res.group(2)
+                    new_line = re.sub(rb'([^A-Za-z0-9])(?<!case )USB_1902([^A-Za-z0-9])', rb'\1USB_1901\2', line)
+                    fp2.write(new_line)
+                    print(line, '->', new_line)
                 else:
                     fp2.write(line)
     
