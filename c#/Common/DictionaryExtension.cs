e1db322509b47862543404b67e9e2269297a55ab
myd7349 <myd7349@gmail.com>
2018-04-17 20:51:21 +0800

    Add DictionaryExtension.cs, StringExtension.cs

---
+++ c#/Common/DictionaryExtension.cs
@@ -0,0 +1,27 @@
+﻿namespace Common
+{
+    using System.Collections.Generic;
+
+    public static class DictionaryExtension
+    {
+        public static TValue GetValueOrDefault<TKey, TValue>(this IDictionary<TKey, TValue> dictionary, TKey key)
+        {
+            TValue value;
+            dictionary.TryGetValue(key, out value);
+            return value;
+        }
+
+        public static TValue GetValueOrDefault<TKey, TValue>(this IDictionary<TKey, TValue> dictionary, TKey key, TValue defaultValue)
+        {
+            TValue value;
+            if (dictionary.TryGetValue(key, out value))
+                return value;
+            return defaultValue;
+        }
+    }
+}
+
+
+// References:
+// https://stackoverflow.com/questions/538729/is-there-an-idictionary-implementation-that-on-missing-key-returns-the-default#
+// https://stackoverflow.com/questions/2601477/dictionary-returning-a-default-value-if-the-key-does-not-exist
---
+++ c#/Common/StringExtension.cs
@@ -0,0 +1,28 @@
+﻿namespace Common
+{
+    using System.Diagnostics;
+
+    public static class StringExtension
+    {
+        public static bool IsNull(this string str)
+        {
+            return str == null;
+        }
+
+        public static bool IsEmpty(this string str)
+        {
+            Debug.Assert(!str.IsNull());
+            return string.IsNullOrEmpty(str);
+        }
+
+        public static bool IsNullOrEmpty(this string str)
+        {
+            return string.IsNullOrEmpty(str);
+        }
+
+        public static bool IsNullOrWhiteSpace(this string str)
+        {
+            return string.IsNullOrWhiteSpace(str);
+        }
+    }
+}
