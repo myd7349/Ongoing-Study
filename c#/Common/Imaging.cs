cdd1f06514044c559cee780d7050d857fadaed5d
myd7349 <myd7349@gmail.com>
2018-04-20 18:00:58 +0800

    Add Imaging.cs, DateTime.linq

---
+++ c#/Common/Imaging.cs
@@ -0,0 +1,37 @@
+﻿namespace Common.Auxiliary.Drawing
+{
+    using System;
+    using System.Drawing.Imaging;
+
+    public static class Imaging
+    {
+        public static ImageFormat GetImageFormat(string fileExtension)
+        {
+            if (string.IsNullOrEmpty(fileExtension))
+                throw new ArgumentException("Invalid file extension");
+
+            if (fileExtension[0] != '.')
+                fileExtension = "." + fileExtension;
+
+            switch (fileExtension.ToLower())
+            {
+                case @".bmp": return ImageFormat.Bmp;
+                case @".emf": return ImageFormat.Emf;
+                case @".gif": return ImageFormat.Gif;
+                case @".ico": return ImageFormat.Icon;
+                case @".jpg":
+                case @".jpeg": return ImageFormat.Jpeg;
+                case @".png": return ImageFormat.Png;
+                case @".tif":
+                case @".tiff": return ImageFormat.Tiff;
+                case @".wmf": return ImageFormat.Wmf;
+                default: throw new NotImplementedException();
+            }
+        }
+    }
+}
+
+
+// References:
+// https://stackoverflow.com/questions/1337750/get-imageformat-from-file-extension
+
---
+++ c#/notes/DateTime.linq
@@ -0,0 +1,6 @@
+<Query Kind="Statements" />
+
+Console.WriteLine(DateTime.Now.ToString("yyyy-MM-ddTHH:mm:sszzz"));
+
+// References:
+// https://stackoverflow.com/questions/7003373/how-to-get-datetime-now-in-yyyy-mm-ddthhmmsstzd-format-using-c-sharp
