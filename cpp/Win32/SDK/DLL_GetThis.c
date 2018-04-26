38da1059b0fa15be70f7350fa5d9a19e9ba73b92
myd7349 <myd7349@gmail.com>
2018-04-26 17:46:07 +0800

    Add DLL_GetThis

---
+++ cpp/Win32/SDK/DLL_GetThis.c
@@ -0,0 +1,21 @@
+#include <tchar.h>
+#include <Windows.h>
+
+
+HMODULE DLL_GetThis()
+{
+    HMODULE hDLLThis = NULL;
+    TCHAR szDir[MAX_PATH] = _T("");
+
+    if(GetModuleHandleEx(
+        GET_MODULE_HANDLE_EX_FLAG_FROM_ADDRESS |
+        GET_MODULE_HANDLE_EX_FLAG_UNCHANGED_REFCOUNT,
+        szDir, &hDLLThis))
+        return hDLLThis;
+
+    return NULL;
+}
+
+
+// References:
+// https://stackoverflow.com/questions/6924195/get-dll-path-at-runtime
---
+++ cpp/Win32/SDK/DLL_GetThis.h
@@ -0,0 +1,11 @@
+#pragma once
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+HMODULE DLL_GetThis();
+
+#ifdef __cplusplus
+}
+#endif
