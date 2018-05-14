8f3cd19851287dff5a6552f3fcc105b71cb11766
myd7349 <myd7349@gmail.com>
2018-05-14 11:28:43 +0800

    Add Split

--- c#/Common/StringExtension.cs
+++ c#/Common/StringExtension.cs
@@ -1,5 +1,6 @@
 ﻿namespace Common
 {
+    using System;
     using System.Diagnostics;
 
     public static class StringExtension
@@ -24,5 +25,20 @@
         {
             return string.IsNullOrWhiteSpace(str);
         }
+
+        public static string[] Split(this string str, string separator, int count, StringSplitOptions options)
+        {
+            Debug.Assert(str != null);
+
+            if (str.Length == 1)
+                return str.Split(new char[] { str[0] }, count, options);
+            else
+                return str.Split(new string[] { str }, count, options);
+        }
     }
 }
+
+
+// References:
+// https://stackoverflow.com/questions/2245442/c-sharp-split-a-string-by-another-string
+// https://zhuanlan.zhihu.com/p/30653282
