088691173835d484a1e3f2dcf32fbab5454be6d5
myd7349 <myd7349@gmail.com>
2018-06-09 13:45:42 +0800

    Add WindowsVersion.h

---
+++ c/Win32/WindowsVersion.c
@@ -0,0 +1,101 @@
+#include "WindowsVersion.h"
+
+// https://github.com/processhacker/processhacker/blob/43f99d5a351a5091dffecdbaa8f22bb10f53dce3/lib/lib32/ntdll.lib
+#pragma comment(lib, "ntdll.lib")
+
+
+WindowsVersion GetWindowsVersion(void)
+{
+    RTL_OSVERSIONINFOEXW versionInfo;
+    ULONG majorVersion;
+    ULONG minorVersion;
+    ULONG buildVersion;
+
+    versionInfo.dwOSVersionInfoSize = sizeof(RTL_OSVERSIONINFOEXW);
+
+    if (!NT_SUCCESS(RtlGetVersion((PRTL_OSVERSIONINFOW)&versionInfo)))
+    {
+        return WINDOWS_NEW;
+    }
+
+    majorVersion = versionInfo.dwMajorVersion;
+    minorVersion = versionInfo.dwMinorVersion;
+    buildVersion = versionInfo.dwBuildNumber;
+
+    if (majorVersion == 5 && minorVersion < 1 || majorVersion < 5)
+    {
+        return WINDOWS_ANCIENT;
+    }
+    /* Windows XP */
+    else if (majorVersion == 5 && minorVersion == 1)
+    {
+        return WINDOWS_XP;
+    }
+    /* Windows Server 2003 */
+    else if (majorVersion == 5 && minorVersion == 2)
+    {
+        return WINDOWS_SERVER_2003;
+    }
+    /* Windows Vista, Windows Server 2008 */
+    else if (majorVersion == 6 && minorVersion == 0)
+    {
+        return WINDOWS_VISTA;
+    }
+    /* Windows 7, Windows Server 2008 R2 */
+    else if (majorVersion == 6 && minorVersion == 1)
+    {
+        return WINDOWS_7;
+    }
+    // Windows 8, Windows Server 2012
+    else if (majorVersion == 6 && minorVersion == 2)
+    {
+        return WINDOWS_8;
+    }
+    // Windows 8.1, Windows Server 2012 R2
+    else if (majorVersion == 6 && minorVersion == 3)
+    {
+        return WINDOWS_8_1;
+    }
+    // Windows 10, Windows Server 2016
+    else if (majorVersion == 10 && minorVersion == 0)
+    {
+        switch (buildVersion)
+        {
+        case 10240:
+            return WINDOWS_10;
+            break;
+        case 10586:
+            return WINDOWS_10_TH2;
+            break;
+        case 14393:
+            return WINDOWS_10_RS1;
+            break;
+        case 15063:
+            return WINDOWS_10_RS2;
+            break;
+        case 16299:
+            return WINDOWS_10_RS3;
+            break;
+        case 17134:
+            return WINDOWS_10_RS4;
+            break;
+        default:
+            return buildVersion > 17134 ? WINDOWS_10_RS4 : WINDOWS_10;
+            break;
+        }
+    }
+    else
+    {
+        return WINDOWS_NEW;
+    }
+}
+
+
+// References:
+// https://github.com/processhacker/processhacker/blob/master/phlib/global.c#160
+//     PhInitializeWindowsVersion
+//
+// http://www.cppblog.com/free2000fly/archive/2008/07/23/56904.html
+// https://msdn.microsoft.com/en-us/library/windows/desktop/ms724429(v=vs.85).aspx
+// https://stackoverflow.com/questions/457872/how-do-you-check-the-windows-version-in-win32-at-runtime
+// https://stackoverflow.com/questions/34235151/get-windows-version
---
+++ c/Win32/WindowsVersion.h
@@ -0,0 +1,43 @@
+#pragma once
+
+#define WIN32_LEAN_AND_MEAN
+#include <Windows.h>
+
+
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+
+typedef __success(return >= 0) LONG NTSTATUS;
+
+#define NT_SUCCESS(Status) (((NTSTATUS)(Status)) >= 0)
+
+
+// https://github.com/processhacker/processhacker/blob/master/phlib/include/phconfig.h#L24
+typedef enum
+{
+    WINDOWS_ANCIENT     = 0,
+    WINDOWS_XP          = 51,
+    WINDOWS_SERVER_2003 = 52,
+    WINDOWS_VISTA       = 60,
+    WINDOWS_7           = 61,
+    WINDOWS_8           = 62,
+    WINDOWS_8_1         = 63,
+    WINDOWS_10          = 100, // TH1
+    WINDOWS_10_TH2      = 101,
+    WINDOWS_10_RS1      = 102,
+    WINDOWS_10_RS2      = 103,
+    WINDOWS_10_RS3      = 104,
+    WINDOWS_10_RS4      = 105,
+    WINDOWS_NEW         = 0x7fffffff
+} WindowsVersion;
+
+NTSYSAPI NTSTATUS NTAPI RtlGetVersion(__out PRTL_OSVERSIONINFOW lpVersionInformation);
+WindowsVersion GetWindowsVersion(void);
+
+#define WINDOWS_HAS_UAC (GetWindowsVersion() >= WINDOWS_VISTA)
+
+#ifdef __cplusplus
+}
+#endif
