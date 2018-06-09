e97ea663d0be96e2eafa416cc45f62dbeabde28b
myd7349 <myd7349@gmail.com>
2018-06-09 21:19:15 +0800

    Add WindowsVersionHelpers.h/c

---
+++ c/Win32/WindowsVersionHelpers.c
@@ -0,0 +1,46 @@
+#include "WindowsVersionHelpers.h"
+
+
+BOOL IsWinVersionOrGreater(DWORD id, WORD wServicePackMajor)
+{
+    WORD wMajorVersion = HIBYTE(id);
+    WORD wMinorVersion = LOBYTE(id);
+
+    OSVERSIONINFOEXW osvi = { sizeof(osvi), 0, 0, 0, 0,{ 0 }, 0, 0 };
+    DWORDLONG const dwlConditionMask =
+        VerSetConditionMask(
+            VerSetConditionMask(
+                VerSetConditionMask(0, VER_MAJORVERSION, VER_GREATER_EQUAL),
+                VER_MINORVERSION, VER_GREATER_EQUAL),
+            VER_SERVICEPACKMAJOR, VER_GREATER_EQUAL);
+
+    osvi.dwMajorVersion = wMajorVersion;
+    osvi.dwMinorVersion = wMinorVersion;
+    osvi.wServicePackMajor = wServicePackMajor;
+
+    return VerifyVersionInfoW(&osvi, VER_MAJORVERSION | VER_MINORVERSION | VER_SERVICEPACKMAJOR, dwlConditionMask) != FALSE;
+}
+
+BOOL IsWindowsXPOrGreater()      {  return IsWinVersionOrGreater(WINDOWS_WINXP, 0);}
+BOOL IsWindowsXPSP1OrGreater()   {  return IsWinVersionOrGreater(WINDOWS_WINXP, 1);}
+BOOL IsWindowsXPSP2OrGreater()   {  return IsWinVersionOrGreater(WINDOWS_WINXP, 2);}
+BOOL IsWindowsXPSP3OrGreater()   {  return IsWinVersionOrGreater(WINDOWS_WINXP, 3);}
+BOOL IsWindowsVistaOrGreater()   {  return IsWinVersionOrGreater(WINDOWS_VISTA, 0);}
+BOOL IsWindowsVistaSP1OrGreater(){  return IsWinVersionOrGreater(WINDOWS_VISTA, 1);}
+BOOL IsWindowsVistaSP2OrGreater(){  return IsWinVersionOrGreater(WINDOWS_VISTA, 2);}
+BOOL IsWindows7OrGreater()       {  return IsWinVersionOrGreater(WINDOWS_WIN7,  0);}
+BOOL IsWindows7SP1OrGreater()    {  return IsWinVersionOrGreater(WINDOWS_WIN7,  1);}
+BOOL IsWindows8OrGreater()       {  return IsWinVersionOrGreater(WINDOWS_WIN8,  0);}
+BOOL IsWindows8Point1OrGreater() {  return IsWinVersionOrGreater(WINDOWS_WINBLUE, 0); }
+BOOL IsWindows10OrGreater()      {  return IsWinVersionOrGreater(WINDOWS_WIN10, 0); }
+
+BOOL IsWindowsServer()
+{
+    OSVERSIONINFOEXW osvi = { sizeof(osvi), 0, 0, 0, 0,{ 0 }, 0, 0, 0, VER_NT_WORKSTATION };
+    DWORDLONG const dwlConditionMask = VerSetConditionMask(0, VER_PRODUCT_TYPE, VER_EQUAL);
+    return !VerifyVersionInfoW(&osvi, VER_PRODUCT_TYPE, dwlConditionMask);
+}
+
+
+// References:
+// https://stackoverflow.com/questions/34235151/get-windows-version
---
+++ c/Win32/WindowsVersionHelpers.h
@@ -0,0 +1,44 @@
+#pragma once
+
+#define WIN32_LEAN_AND_MEAN
+#include <Windows.h>
+
+
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+
+#define WINDOWS_NT4      0x0400
+#define WINDOWS_WIN2K    0x0500
+#define WINDOWS_WINXP    0x0501
+#define WINDOWS_WS03     0x0502
+#define WINDOWS_WIN6     0x0600
+#define WINDOWS_VISTA    0x0600
+#define WINDOWS_WS08     0x0600
+#define WINDOWS_LONGHORN 0x0600
+#define WINDOWS_WIN7     0x0601
+#define WINDOWS_WIN8     0x0602
+#define WINDOWS_WINBLUE  0x0603
+#define WINDOWS_WIN10    0x0A00
+
+BOOL IsWindowsXPOrGreater();
+BOOL IsWindowsXPSP1OrGreater();
+BOOL IsWindowsXPSP2OrGreater();
+BOOL IsWindowsXPSP3OrGreater();
+BOOL IsWindowsVistaOrGreater();
+BOOL IsWindowsVistaSP1OrGreater();
+BOOL IsWindowsVistaSP2OrGreater();
+BOOL IsWindows7OrGreater();
+BOOL IsWindows7SP1OrGreater();
+BOOL IsWindows8OrGreater();
+BOOL IsWindows8Point1OrGreater();
+BOOL IsWindows10OrGreater();
+
+BOOL IsWindowsServer();
+
+#define WINDOWS_HAS_UAC (IsWindowsVistaOrGreater() >= WINDOWS_VISTA)
+
+#ifdef __cplusplus
+}
+#endif
