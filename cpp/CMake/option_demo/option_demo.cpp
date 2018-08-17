0518488fb8b3f393bf1863851fcf3921d5d30047
myd7349 <myd7349@gmail.com>
2018-08-17 12:04:04 +0800

    Update option_demo.cpp

--- cpp/CMake/option_demo/option_demo.cpp
+++ cpp/CMake/option_demo/option_demo.cpp
@@ -2,14 +2,10 @@
 
 #include <iostream>
 
-#ifndef OLD_SCHOOL_STYLE
-# define OLD_SCHOOL_STYLE (0)
-#endif
-
 
 int main()
 {
-#if OLD_SCHOOL_STYLE
+#ifdef OLD_SCHOOL_STYLE
     std::cout << "Hello, school!\n";
 #else
     std::cout << "Hello, world!\n";
