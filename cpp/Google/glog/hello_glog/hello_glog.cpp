755def648a663ca582a54257f65fbdc9a335f161
myd7349 <myd7349@gmail.com>
2018-09-13 21:04:18 +0800

    Add hello_glog

---
+++ cpp/Google/glog/hello_glog/hello_glog.cpp
@@ -0,0 +1,29 @@
+#include <vector>
+
+#include <glog/logging.h>
+#include <glog/stl_logging.h>
+
+
+int main(int argc, char *argv[])
+{
+    google::InitGoogleLogging(argv[0]);
+    gflags::ParseCommandLineFlags(&argc, &argv, true);
+
+    google::SetLogFilenameExtension(".log");
+    google::SetLogDestination(0, "hello_glog");
+    google::SetLogDestination(google::GLOG_WARNING, "");
+
+    FLAGS_alsologtostderr = 1;
+
+    LOG(INFO) << "Hello, world!";
+    LOG(WARNING) << "This is a test code. This is a real life.";
+    LOG(ERROR) << "You can not see me!";
+
+    std::vector<int> fibs { 1, 1, 2, 3, 5, 8, 13, 21, 34, 55 };
+    LOG(INFO) << fibs;
+
+    return 0;
+}
+
+// References:
+// https://stackoverflow.com/questions/20016036/glog-no-output-file-log-is-created
