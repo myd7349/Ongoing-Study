c72a2e79985de578e58e71ed65d36ff7b4ea2ab9
myd7349 <myd7349@gmail.com>
2018-11-24 17:10:59 +0800

    Add hello_rapidstring

---
+++ c/rapidstring/hello_rapidstring/hello_rapidstring.c
@@ -0,0 +1,15 @@
+﻿#include <stdio.h>
+
+#include <rapidstring.h>
+
+int main()
+{
+    rapidstring s;
+
+    rs_init_w(&s, "Hello, world!");
+
+    puts(rs_data(&s));
+    printf("%zu\n", rs_len(&s));
+
+    return 0;
+}
