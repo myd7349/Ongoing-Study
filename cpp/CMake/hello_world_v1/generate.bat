075ecaf580f2e68e3404d397c691daf1cdc304a1
myd7349 <myd7349@gmail.com>
2018-12-17 12:33:31 +0800

    Add hello_world_v1

---
+++ cpp/CMake/hello_world_v1/generate.bat
@@ -0,0 +1,4 @@
+cd /d "%~dp0"
+md build
+cd build
+cmake ..
---
+++ cpp/CMake/hello_world_v1/src/main.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(void)
+{
+    puts("Hello, world!");
+    return 0;
+}
+
+// Referepces:
+// CMake Practice.pdf
