6115352b25ba8625511cd4b78fadaca6cfe02b9d
myd7349 <myd7349@gmail.com>
2018-12-24 20:41:18 +0800

    Update erase_items_while_iterating

--- cpp/erase_items_while_iterating/main.cpp
+++ cpp/erase_items_while_iterating/main.cpp
@@ -1,5 +1,6 @@
 // 2016-08-05T21:50+08:00
 #include <iostream>
+#include <iterator>
 #include <list>
 #include <vector>
 
@@ -10,12 +11,46 @@ int main()
     std::list<int> lst { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
 
     // Erase even numbers in this list while iterating through it.
+#if 0
     for (auto it = lst.begin(); it != lst.end(); )
     {
         if (*it % 2 == 0)
             lst.erase(it++);
         else
             ++it;
+    }
+#else
+    for (auto it = lst.begin(); it != lst.end();)
+    {
+        if (*it % 2 == 0)
+            it = lst.erase(it);
+        else
+            ++it;
+    }
+#endif
+
+    Println(lst);
+
+    lst = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+
+    // An example from C++ Primer
+    // Remove even-valued elements and insert a duplicate of odd-valued elements
+    auto it = lst.begin();
+    while (it != lst.end())
+    {
+        if (*it % 2 != 0)
+        {
+            // >insert inserts before the position it is given and returns
+            // an iterator to the inserted element.
+            it = lst.insert(it, *it);
+            std::advance(it, 2);
+        }
+        else
+        {
+            // >the iterator returned from erase denotes the
+            // next element in the sequence.
+            it = lst.erase(it);
+        }
     }
 
     Println(lst);
@@ -40,3 +75,4 @@ int main()
 // [Can you remove elements from a std::list while iterating through it?](http://www.stackoverflow.com/questions/596162/can-you-remove-elements-from-a-stdlist-while-iterating-through-it)
 // [Deleting elements from STL set while iterating](http://www.stackoverflow.com/questions/2874441/deleting-elements-from-stl-set-while-iterating)
 // [Erasing while iterating an std::list](http://www.stackoverflow.com/questions/16269696/erasing-while-iterating-an-stdlist)
+// C++ Primer, 5th Edition, Writing Loops That Change a Container
