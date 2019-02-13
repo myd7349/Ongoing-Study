8e054895ce4fd046b2e5b961504d7ef17a6c139c
myd7349 <myd7349@gmail.com>
2019-02-13 18:26:04 +0800

    Add zversion, find_libzmq_ng

---
+++ cpp/CMake/find_libzmq_ng/build.bat
@@ -0,0 +1,5 @@
+mkdir build
+cd build
+cmake .. -DCMAKE_TOOLCHAIN_FILE="%VCPKG_ROOT%\scripts\buildsystems\vcpkg.cmake" -DVCPKG_TARGET_TRIPLET=x86-windows -DCMAKE_GENERATOR_PLATFORM=Win32
+cd ..
+pause
