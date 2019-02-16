260b712f368bc9650fd3b80ef2042e966f85229a
myd7349 <myd7349@gmail.com>
2019-02-16 17:35:20 +0800

    Update xeus_example

---
+++ cpp/CMake/vcpkg/xeus_example/build-x86-windows-static.bat
@@ -0,0 +1,6 @@
+mkdir build-x86-windows-static
+cd build-x86-windows-static
+cmake .. -DCMAKE_TOOLCHAIN_FILE="%VCPKG_ROOT%\scripts\buildsystems\vcpkg.cmake" -DVCPKG_TARGET_TRIPLET=x86-windows-static -DCMAKE_GENERATOR_PLATFORM=Win32 -DUSE_STATIC_RTL=ON
+cmake --build . --config Release
+cd ..
+pause
