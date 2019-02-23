3f7dc84436fa0cc8a0fde2cdf54952831f016230
myd7349 <myd7349@gmail.com>
2019-02-23 17:15:40 +0800

    Add stxxl_copy_file_example

---
+++ cpp/CMake/vcpkg/stxxl_copy_file_example/build-x86-windows.bat
@@ -0,0 +1,6 @@
+mkdir build-x86-windows
+cd build-x86-windows
+cmake .. -DCMAKE_TOOLCHAIN_FILE="%VCPKG_ROOT%\scripts\buildsystems\vcpkg.cmake" -DVCPKG_TARGET_TRIPLET=x86-windows -DCMAKE_GENERATOR_PLATFORM=Win32 -DUSE_STATIC_RTL=ON
+cmake --build . --config Release
+cd ..
+pause
