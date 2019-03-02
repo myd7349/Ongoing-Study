56b0000582d55901e60ed6b4a89c53acff91da48
myd7349 <myd7349@gmail.com>
2019-03-02 20:29:03 +0800

    Add tiny_process_library_example

---
+++ cpp/CMake/vcpkg/tiny_process_library_example/build-x86-windows-static.bat
@@ -0,0 +1,9 @@
+mkdir build-x86-windows-static
+cd build-x86-windows-static
+cmake .. -DCMAKE_TOOLCHAIN_FILE="%VCPKG_ROOT%\scripts\buildsystems\vcpkg.cmake" -DVCPKG_TARGET_TRIPLET=x86-windows-static -DCMAKE_GENERATOR_PLATFORM=Win32 -DUSE_STATIC_RTL=ON
+cmake --build . --config Release
+cd Release
+tell_me.exe
+cd ..
+cd ..
+pause
---
+++ cpp/CMake/vcpkg/tiny_process_library_example/tell_me.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+#include <process.hpp>
+
+
+int main()
+{
+    TinyProcessLib::Process proc("answer");
+    auto answer = proc.get_exit_status();
+    std::cout << "The answer: " << answer << std::endl;
+    
+    return 0;
+}
