1f63e8646be39b4413a72ff91360ea7958896007
myd7349 <myd7349@gmail.com>
2019-03-03 22:23:54 +0800

    Add OpenFolderAndSelectItems

--- cpp/Win32/MFC/Path.cpp
+++ cpp/Win32/MFC/Path.cpp
@@ -25,3 +25,111 @@ CString ReplaceFileExt(LPCTSTR lpcszFilePath, LPCTSTR lpcszNewExt)
     
     return lpcszFilePath;
 }
+
+
+
+CString GetFileTitle(LPCTSTR lpcszFilePath)
+{
+    ASSERT(lpcszFilePath != NULL);
+
+    TCHAR szFileTitle[MAX_PATH];
+    if (::GetFileTitle(lpcszFilePath, szFileTitle, ARRAYSIZE(szFileTitle)) == 0)
+        return szFileTitle;
+
+    return _T("");
+}
+
+
+BOOL OpenFolderAndSelectItems(LPCTSTR lpcszFilePath)
+{
+    return OpenFolderAndSelectItems(lpcszFilePath, NULL);
+}
+
+
+BOOL OpenFolderAndSelectItems(LPCTSTR lpcszFileDir, LPCTSTR lpcszFileName, ...)
+{
+    ASSERT(lpcszFileDir != NULL);
+    
+    HRESULT hr;
+
+    //hr = CoInitializeEx(0, COINIT_MULTITHREADED);
+    //if (hr != S_OK)
+    //    return FALSE;
+
+    CString strFolder = GetDirName(lpcszFileDir);
+
+    ATL::CPath filePath(lpcszFileDir);
+    if (!filePath.FileExists())
+    {
+        ATL::CPath fileFolder(strFolder);
+        if (!fileFolder.FileExists())
+            return FALSE;
+    }
+
+    // SHSimpleIDListFromPath
+    // SHGetDesktopFolder
+    // https://stackoverflow.com/questions/47564192/shopenfolderandselectitems-with-arrays
+    LPITEMIDLIST lpilFolder = ILCreateFromPath(strFolder);
+    if (lpilFolder == nullptr)
+    {
+        //CoUninitialize();
+        return FALSE;
+    }
+
+    std::vector<LPITEMIDLIST> vpil;
+    if (!filePath.IsDirectory())
+    {
+        LPITEMIDLIST lpil = ILCreateFromPath(lpcszFileDir);
+        if (lpil != nullptr)
+            vpil.push_back(lpil);
+    }
+
+    if (lpcszFileName != NULL)
+    {
+        CString strTemp = JoinPath(strFolder, lpcszFileName);
+        if (ATL::ATLPath::FileExists(strTemp) && !ATL::ATLPath::IsDirectory(strTemp))
+        {
+            LPITEMIDLIST lpil = ILCreateFromPath(strTemp);
+            if (lpil != nullptr)
+                vpil.push_back(lpil);
+        }
+
+        va_list args;
+
+        va_start(args, lpcszFileName);
+
+        while (true)
+        {
+            LPCTSTR lpcszFile = va_arg(args, LPCTSTR);
+            if (lpcszFile == nullptr)
+                break;
+
+            strTemp = JoinPath(strFolder, lpcszFile);
+            if (ATL::ATLPath::FileExists(strTemp) && !ATL::ATLPath::IsDirectory(strTemp))
+            {
+                LPITEMIDLIST lpil = ILCreateFromPath(strTemp);
+                if (lpil != nullptr)
+                    vpil.push_back(lpil);
+            }
+        }
+
+        va_end(args);
+    }
+
+    hr = SHOpenFolderAndSelectItems(
+        lpilFolder,
+        static_cast<UINT>(vpil.size()),
+        const_cast<LPCITEMIDLIST*>(vpil.data()),
+        0
+        );
+
+    for (std::vector<LPITEMIDLIST>::size_type i = 0; i < vpil.size(); ++i)
+        ILFree(vpil[i]);
+
+    ILFree(lpilFolder);
+
+    //CoUninitialize();
+
+    return hr == S_OK;
+}
+
--- cpp/Win32/MFC/Path.h
+++ cpp/Win32/MFC/Path.h
@@ -1,3 +1,9 @@
 #pragma once
 
 CString ReplaceFileExt(LPCTSTR lpcszFilePath, LPCTSTR lpcszNewExt);
+
+CString GetFileTitle(LPCTSTR lpcszFilePath);
+
+BOOL OpenFolderAndSelectItems(LPCTSTR lpcszFilePath);
+
+BOOL OpenFolderAndSelectItems(LPCTSTR lpcszFileDir, LPCTSTR lpcszFileName, ...);
