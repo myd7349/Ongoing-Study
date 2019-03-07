bff2d23538eb62973ebbe1993f56f90588ac8cc0
myd7349 <myd7349@gmail.com>
2019-03-07 20:11:01 +0800

    Update QuadraticDemo

---
+++ cpp/Qt/QCustomPlot/QuadraticDemo/build-x86-windows-static.bat
@@ -0,0 +1,7 @@
+mkdir build-x86-windows-static
+cd build-x86-windows-static
+cmake .. -DCMAKE_TOOLCHAIN_FILE="%VCPKG_ROOT%\scripts\buildsystems\vcpkg.cmake" -DVCPKG_TARGET_TRIPLET=x86-windows-static -DCMAKE_GENERATOR_PLATFORM=Win32 -DUSE_STATIC_CRT=ON
+cmake --build . --config Release
+Release\QuadraticDemo.exe
+cd ..
+pause
---
+++ cpp/Qt/QCustomPlot/QuadraticDemo/build-x86-windows.bat
@@ -0,0 +1,7 @@
+mkdir build-x86-windows
+cd build-x86-windows
+cmake .. -DCMAKE_TOOLCHAIN_FILE="%VCPKG_ROOT%\scripts\buildsystems\vcpkg.cmake" -DVCPKG_TARGET_TRIPLET=x86-windows -DCMAKE_GENERATOR_PLATFORM=Win32
+cmake --build . --config Release
+Release\QuadraticDemo.exe
+cd ..
+pause
