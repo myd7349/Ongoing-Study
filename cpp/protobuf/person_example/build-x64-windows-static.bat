aee5ac38d108ac6adbf1ecb7f421b0722795c782
myd7349 <myd7349@gmail.com>
2019-03-19 19:06:58 +0800

    Update person_example

--- cpp/protobuf/person_example/build-x64-windows-static.bat
+++ cpp/protobuf/person_example/build-x64-windows-static.bat
@@ -1,3 +1,4 @@
+vcpkg install protobuf:x64-windows-static
 mkdir build-x64-windows-static
 cd build-x64-windows-static
 cmake .. -DCMAKE_TOOLCHAIN_FILE="%VCPKG_ROOT%\scripts\buildsystems\vcpkg.cmake" -DVCPKG_TARGET_TRIPLET=x64-windows-static -DCMAKE_GENERATOR_PLATFORM=x64
---
+++ cpp/protobuf/person_example/install-windows.bat
@@ -0,0 +1,2 @@
+vcpkg install protobuf:x86-windows protobuf:x64-windows protobuf:x86-windows-static protobuf:x64-windows-static protobuf:x86-uwp protobuf:x64-uwp
+pause
