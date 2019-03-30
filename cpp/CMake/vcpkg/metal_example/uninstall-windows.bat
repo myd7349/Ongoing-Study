401dde9bd64286c5602d4a17b16d3c3390d886d6
myd7349 <myd7349@gmail.com>
2019-03-30 16:31:37 +0800

    Add metal_example

---
+++ cpp/CMake/vcpkg/metal_example/uninstall-windows.bat
@@ -0,0 +1,2 @@
+vcpkg remove brunocodutra-metal:x86-windows brunocodutra-metal:x64-windows brunocodutra-metal:x86-windows-static brunocodutra-metal:x64-windows-static brunocodutra-metal:x86-uwp brunocodutra-metal:x64-uwp
+pause
