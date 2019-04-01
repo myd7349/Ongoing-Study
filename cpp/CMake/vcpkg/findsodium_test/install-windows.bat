31976fe14241994717e5aebd39599571d1f0abd3
myd7349 <myd7349@gmail.com>
2019-04-01 18:39:22 +0800

    Add findsodium_test

---
+++ cpp/CMake/vcpkg/findsodium_test/install-windows.bat
@@ -0,0 +1,2 @@
+vcpkg install libsodium:x86-windows libsodium:x64-windows libsodium:x86-windows-static libsodium:x64-windows-static libsodium:x86-uwp libsodium:x64-uwp
+pause
---
+++ cpp/CMake/vcpkg/libxml2_test/uninstall-windows.bat
@@ -0,0 +1,2 @@
+vcpkg remove libxml2:x86-windows libxml2:x64-windows libxml2:x86-windows-static libxml2:x64-windows-static libxml2:x86-uwp libxml2:x64-uwp
+pause
