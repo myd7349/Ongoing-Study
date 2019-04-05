3f76ba3aa0b74e06f3337c1a0bd826f472617c04
myd7349 <myd7349@gmail.com>
2019-04-05 17:56:15 +0800

    Update g3log_example

--- cpp/CMake/vcpkg/g3log_example/install-windows.bat
+++ cpp/CMake/vcpkg/g3log_example/install-windows.bat
@@ -1,2 +1,3 @@
-vcpkg install g3log:x86-windows g3log:x64-windows g3log:x86-windows-static g3log:x64-windows-static g3log:x86-uwp g3log:x64-uwp
+vcpkg install g3log:x86-windows g3log:x64-windows g3log:x86-windows-static g3log:x64-windows-static
+:: g3log:x86-uwp g3log:x64-uwp
 pause
