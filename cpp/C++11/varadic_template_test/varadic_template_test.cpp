39de20f0a3f0e1cc9bae5ce8c947126824e66622
myd7349 <myd7349@gmail.com>
2019-04-06 10:16:45 +0800

    Update varadic_template_test

--- cpp/C++11/varadic_template_test/varadic_template_test.cpp
+++ cpp/C++11/varadic_template_test/varadic_template_test.cpp
@@ -27,11 +27,48 @@ void Log(TFirst &&first, TParams &&...params)
     //static int ID = 0;
 
     std::ostringstream oss;
-    oss << "Out [" << ++ID << "]: ";
+    oss << "Out [" << ++ID << "] <" << sizeof...(params) << ">: ";
     LogImpl(oss, first, std::forward<TParams>(params)...);
     oss << std::endl;
 
     std::cout << oss.str();
+}
+
+
+template <typename T>
+std::ostream &Print(std::ostream &os, const T &t)
+{
+    return os << t;
+}
+
+
+template <typename T, typename... TArgs>
+std::ostream &Print(std::ostream &os, const T &t, const TArgs&... rest)
+{
+    os << t << ", ";
+    return Print(os, rest...);
+}
+
+
+template <typename T, typename... TArgs>
+std::ostream &Println(std::ostream &os, const T &t, const TArgs&... rest)
+{
+    os << t << ", ";
+    return Print(os, rest...) << '\n';
+}
+
+
+template <typename T>
+inline T Square(T x)
+{
+    return x * x;
+}
+
+
+template <typename... TArgs>
+std::ostream &PrintSquared(std::ostream &os, const TArgs&... rest)
+{
+    return Println(os, Square(rest)...);
 }
 
 
@@ -40,6 +77,12 @@ int main()
     Log("Hello, world!");
     Log("This is a test code!");
     Log("This is a real life!");
+    Log("PI: ", 3.141592653);
+    
+    Println(std::cout, 1, 2, 3, 4);
+    Println(std::cout, 1, 2, 3.14, 42, 5.0);
+
+    PrintSquared(std::cout, 1, 2, 3, 4, 5);
 
     PAUSE();
 
@@ -51,3 +94,4 @@ int main()
 // https://stackoverflow.com/questions/29326460/how-to-make-a-variadic-macro-for-stdcout
 // https://stackoverflow.com/questions/2821223/how-would-one-call-stdforward-on-all-arguments-in-a-variadic-function
 // http://eli.thegreenplace.net/2014/variadic-templates-in-c/
+// C++ Primer, 5th Edition, 16.4 Variadic Templates
