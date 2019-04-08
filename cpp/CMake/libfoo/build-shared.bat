6426775d5c4f381321e99883dac907fa3a23e410
myd7349 <myd7349@gmail.com>
2019-04-08 18:19:48 +0800

    CPack test

---
+++ cpp/CMake/libfoo/build-shared.bat
@@ -0,0 +1,6 @@
+mkdir build-shared
+cd build-shared
+cmake .. -DBUILD_SHARED_LIBS=ON -DBUILD_STATIC_LIBS=OFF
+cmake --build . --config Release
+ctest . -C Release
+pause
---
+++ cpp/CMake/libfoo/build-static.bat
@@ -0,0 +1,6 @@
+mkdir build-static
+cd build-static
+cmake .. -DBUILD_SHARED_LIBS=OFF -DBUILD_STATIC_LIBS=ON
+cmake --build . --config Release
+ctest . -C Release
+pause
