4cd9405cf95ab10de056d272d77d7ad9fb9401e1
myd7349 <myd7349@gmail.com>
2019-04-13 17:00:35 +0800

    Update libfoo

--- cpp/CMake/libfoo/build-shared.bat
+++ cpp/CMake/libfoo/build-shared.bat
@@ -3,4 +3,5 @@ cd build-shared
 cmake .. -DBUILD_SHARED_LIBS=ON -DBUILD_STATIC_LIBS=OFF
 cmake --build . --config Release
 ctest . -C Release
+cd ..
 pause
--- cpp/CMake/libfoo/build-static.bat
+++ cpp/CMake/libfoo/build-static.bat
@@ -3,4 +3,5 @@ cd build-static
 cmake .. -DBUILD_SHARED_LIBS=OFF -DBUILD_STATIC_LIBS=ON
 cmake --build . --config Release
 ctest . -C Release
+cd ..
 pause
