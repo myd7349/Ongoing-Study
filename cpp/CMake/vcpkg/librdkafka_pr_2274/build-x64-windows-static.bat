1bedf70d7862b6d0e4bec3777ec730909708c3f5
myd7349 <myd7349@gmail.com>
2019-05-01 09:51:33 +0800

    Update librdkafka_pr_2274

--- cpp/CMake/vcpkg/librdkafka_pr_2274/build-x64-windows-static.bat
+++ cpp/CMake/vcpkg/librdkafka_pr_2274/build-x64-windows-static.bat
@@ -1,6 +1,6 @@
 vcpkg install librdkafka[lz4,ssl,zlib,zstd]:x64-windows-static
 mkdir build-x64-windows-static
 cd build-x64-windows-static
-cmake .. -DCMAKE_TOOLCHAIN_FILE="%VCPKG_ROOT%\scripts\buildsystems\vcpkg.cmake" -DVCPKG_TARGET_TRIPLET=x64-windows-static -DCMAKE_GENERATOR_PLATFORM=x64
+cmake .. -DCMAKE_TOOLCHAIN_FILE="%VCPKG_ROOT%\scripts\buildsystems\vcpkg.cmake" -DVCPKG_TARGET_TRIPLET=x64-windows-static -DCMAKE_GENERATOR_PLATFORM=x64 -DUSE_MSVC_STATIC_RUNTIME=ON
 cmake --build . --config Release
 pause
--- cpp/CMake/vcpkg/librdkafka_pr_2274/build-x86-windows-static.bat
+++ cpp/CMake/vcpkg/librdkafka_pr_2274/build-x86-windows-static.bat
@@ -1,6 +1,6 @@
 vcpkg install librdkafka[lz4,ssl,zlib,zstd]:x86-windows-static
 mkdir build-x86-windows-static
 cd build-x86-windows-static
-cmake .. -DCMAKE_TOOLCHAIN_FILE="%VCPKG_ROOT%\scripts\buildsystems\vcpkg.cmake" -DVCPKG_TARGET_TRIPLET=x86-windows-static -DCMAKE_GENERATOR_PLATFORM=Win32
+cmake .. -DCMAKE_TOOLCHAIN_FILE="%VCPKG_ROOT%\scripts\buildsystems\vcpkg.cmake" -DVCPKG_TARGET_TRIPLET=x86-windows-static -DCMAKE_GENERATOR_PLATFORM=Win32 -DUSE_MSVC_STATIC_RUNTIME=ON
 cmake --build . --config Release
 pause
