cc6c31c5e6fb500c5bd1bf16dc4576182aa32002
myd7349 <myd7349@gmail.com>
2019-05-11 12:13:24 +0800

    Add sx_example

---
+++ cpp/CMake/vcpkg/sx_example/build-x64-windows.bat
@@ -0,0 +1,9 @@
+vcpkg install septag-sx:x64-windows
+mkdir build-x64-windows
+cd build-x64-windows
+cmake .. -DCMAKE_TOOLCHAIN_FILE="%VCPKG_ROOT%\scripts\buildsystems\vcpkg.cmake" -DVCPKG_TARGET_TRIPLET=x64-windows -DCMAKE_GENERATOR_PLATFORM=x64
+cmake --build . --config Release
+.\Release\test-cmdline.exe -v -i a
+.\Release\test-ini.exe
+cd ..
+pause
---
+++ cpp/CMake/vcpkg/sx_example/install-windows.bat
@@ -0,0 +1,2 @@
+vcpkg install septag-sx:x86-windows septag-sx:x64-windows septag-sx:x86-windows-static septag-sx:x64-windows-static
+pause
