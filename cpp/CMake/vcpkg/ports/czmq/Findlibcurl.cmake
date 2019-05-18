754c894ddcf4b419b58463215a14e1c4783131b0
myd7349 <myd7349@gmail.com>
2019-05-18 15:20:17 +0800

    [czmq] Fix libcurl support on Linux

--- cpp/CMake/vcpkg/ports/czmq/Findlibcurl.cmake
+++ cpp/CMake/vcpkg/ports/czmq/Findlibcurl.cmake
@@ -1,20 +1,38 @@
+find_path(
+    LIBCURL_INCLUDE_DIRS
+    NAMES curl/curl.h
+)
+
+find_library(
+    LIBCURL_LIBRARY_DEBUG
+    NAMES libcurl-d_imp libcurl-d curl-d curl
+)
+
+find_library(
+    LIBCURL_LIBRARY_RELEASE
+    NAMES libcurl_imp libcurl curl
+)
+
+include(SelectLibraryConfigurations)
+select_library_configurations(LIBCURL)
+
+include(FindPackageHandleStandardArgs)
+find_package_handle_standard_args(
+    LIBCURL
+    REQUIRED_VARS LIBCURL_LIBRARIES LIBCURL_INCLUDE_DIRS
+)
+
+# CURL dependencies
 # https://github.com/Microsoft/vcpkg/issues/4312
 # https://github.com/microsoft/vcpkg/commit/ee325d14276e7bd7961e94e58293b02d6e9e92da
-find_package(CURL QUIET)
-
-if (CURL_FOUND)
-    set(LIBCURL_FOUND ${CURL_FOUND})
-    set(LIBCURL_INCLUDE_DIRS ${CURL_INCLUDE_DIRS})
-
+if (LIBCURL_FOUND)
     find_package(OpenSSL REQUIRED)
     find_package(ZLIB REQUIRED)
 
-    set(LIBCURL_LIBRARIES
-        ${CURL_LIBRARIES}
-        # CURL dependencies
+    list(APPEND LIBCURL_LIBRARIES
         OpenSSL::SSL
         OpenSSL::Crypto
-        ZLIB::ZLIB
+        ZLIB::ZLIB    
     )
 
     if (WIN32)
