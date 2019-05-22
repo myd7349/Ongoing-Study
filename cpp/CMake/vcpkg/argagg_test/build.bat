8e4534c5937f327601443c43b1e47412f3c0f52e
myd7349 <myd7349@gmail.com>
2019-05-22 19:03:10 +0800

    Add argagg_test

---
+++ cpp/CMake/vcpkg/argagg_test/build.bat
@@ -0,0 +1,7 @@
+mkdir build
+cd build
+cmake .. -DCMAKE_TOOLCHAIN_FILE="%VCPKG_ROOT%\scripts\buildsystems\vcpkg.cmake" -DVCPKG_TARGET_TRIPLET=x86-windows -DCMAKE_GENERATOR_PLATFORM=Win32
+cmake --build . --config Release
+Release\argagg_test
+cd ..
+pause
