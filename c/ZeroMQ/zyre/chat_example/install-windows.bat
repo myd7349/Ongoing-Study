d2900d6388aae591d75f9ab653a57dbbd5254035
myd7349 <myd7349@gmail.com>
2019-07-07 17:21:09 +0800

    Add chat_example

---
+++ c/ZeroMQ/zyre/chat_example/install-windows.bat
@@ -0,0 +1,2 @@
+vcpkg install zyre:x86-windows zyre:x64-windows zyre:x86-windows-static zyre:x64-windows-static
+pause
---
+++ c/ZeroMQ/zyre/chat_example/uninstall-windows.bat
@@ -0,0 +1,2 @@
+vcpkg remove zyre:x86-windows zyre:x64-windows zyre:x86-windows-static zyre:x64-windows-static zyre:x86-uwp zyre:x64-uwp
+pause
