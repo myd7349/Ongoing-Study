9f6ca68ca5b4f3dea5d5cca002a9fd68303bed08
myd7349 <myd7349@gmail.com>
2019-07-21 19:07:44 +0800

    Add cmcstl2_test

---
+++ cpp/CMake/vcpkg/cmcstl2_test/build.bat
@@ -0,0 +1,8 @@
+vcpkg install cmcstl2
+mkdir build
+cd build
+cmake .. -DCMAKE_TOOLCHAIN_FILE="%VCPKG_ROOT%\scripts\buildsystems\vcpkg.cmake" -DVCPKG_TARGET_TRIPLET=x86-windows -DCMAKE_GENERATOR_PLATFORM=Win32
+cmake --build . --config Release
+Release\pprint_test
+cd ..
+pause
