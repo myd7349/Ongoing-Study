0473b2cd67cd4fcb914c19e479720540a4c60f29
myd7349 <myd7349@gmail.com>
2019-07-23 22:02:39 +0800

    Add DrawArrow

---
+++ c#/WinForm/DrawArrow/DrawArrow/DrawArrowForm.cs
@@ -0,0 +1,82 @@
+﻿namespace DrawArrow
+{
+    using System.Drawing;
+    using System.Drawing.Drawing2D;
+    using System.Drawing.Imaging;
+    using System.Windows.Forms;
+
+    public partial class DrawArrowForm : Form
+    {
+        public DrawArrowForm()
+        {
+            InitializeComponent();
+        }
+
+        private void DrawArrowForm_Paint(object sender, PaintEventArgs e)
+        {
+            e.Graphics.FillRectangle(Brushes.SkyBlue, ClientRectangle);
+
+            var arrowBitmap = Properties.Resources.arrow;
+
+            e.Graphics.DrawImage(arrowBitmap,
+                new Rectangle
+                {
+                    X = 10,
+                    Y = 10,
+                    Width = 75,
+                    Height = 100
+                }
+                );
+
+            var colors = new Color[]
+            {
+                Color.White,
+                Color.Red,
+                Color.Orange,
+                Color.Yellow,
+                Color.Green,
+                Color.Cyan,
+                Color.Blue,
+                Color.Purple,
+                Color.GreenYellow,
+                Color.Goldenrod,
+                Color.HotPink,
+                Color.IndianRed,
+            };
+
+            var rectangle = new Rectangle
+            {
+                X = 10,
+                Y = 10,
+                Width = 75,
+                Height = 100
+            };
+
+            var xOffset = 50;
+
+            var colorMap = new ColorMap[1];
+            colorMap[0] = new ColorMap();
+
+            var imageAttribute = new ImageAttributes();
+
+            foreach (var color in colors)
+            {
+                colorMap[0].OldColor = Color.Black;
+                colorMap[0].NewColor = color;
+
+                imageAttribute.SetRemapTable(colorMap);
+
+                rectangle.X += xOffset;
+
+                e.Graphics.DrawImage(arrowBitmap,
+                    rectangle, 0, 0, arrowBitmap.Width, arrowBitmap.Height,
+                    GraphicsUnit.Pixel, imageAttribute
+                    );
+            }
+        }
+    }
+}
+
+
+// References:
+// https://social.msdn.microsoft.com/Forums/vstudio/en-US/44c21755-28c3-4519-aff2-0565e88fceb2/changing-specific-color-of-an-image-in-picturebox-in-c
