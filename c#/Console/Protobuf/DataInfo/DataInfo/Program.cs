670c288af970e5309cae16ef8307f93c016f8250
myd7349 <myd7349@gmail.com>
2019-08-14 18:52:39 +0800

    Add protobuf, protobuf-net examples

---
+++ c#/Console/Protobuf/DataInfo/DataInfo/Program.cs
@@ -0,0 +1,55 @@
+﻿// Install-Package Google.Protobuf -Version 3.9.1
+// Install-Package Newtonsoft.Json -Version 12.0.2
+
+namespace DataInfo
+{
+    using static System.Console;
+
+    using pb = Google.Protobuf;
+    using Newtonsoft.Json;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var dataMetaInfo = new DataModel.DataMetaInfo
+            {
+                JobInfo = new DataModel.JobInfo
+                {
+                    Name = "R&D",
+                    Department = "Tech. Department",
+                },
+                PersonInfo = new DataModel.PersonInfo
+                {
+                    Name = "Tom",
+                    Age = 22,
+                    Gender = DataModel.PersonInfo.Types.Gender.Male,
+                },
+                Channels = 20,
+                SamplingRate = 16000,
+                DataWidth = DataModel.DataMetaInfo.Types.DataWidth.Custom,
+                CustomDataWidth = 6,
+            };
+
+            var json = JsonConvert.SerializeObject(dataMetaInfo, Formatting.Indented);
+            WriteLine(json);
+
+            var json2 = new pb.JsonFormatter(pb.JsonFormatter.Settings.Default).Format(dataMetaInfo);
+            WriteLine(json2);
+
+            var jsonParserSettings = new pb.JsonParser.Settings(10).WithIgnoreUnknownFields(true);
+            var dataMetaInfo2 = new pb.JsonParser(jsonParserSettings).Parse<DataModel.DataMetaInfo>(json);
+            var json3 = JsonConvert.SerializeObject(dataMetaInfo2, Formatting.Indented);
+            WriteLine(json3);
+            WriteLine(json == json3);
+
+            ReadKey();
+        }
+    }
+}
+
+
+// References:
+// https://stackoverflow.com/questions/39907232/compiling-proto3-files-in-vs2015-c-sharp-project
+// https://stackoverflow.com/questions/49791478/use-protobuf-net-to-convert-json-into-object
+// https://damienbod.com/2014/01/09/comparing-protobuf-json-bson-xml-with-net-for-file-streams/
---
+++ c#/Console/protobuf-net/PersonExample/PersonExample/Program.cs
@@ -0,0 +1,65 @@
+﻿namespace PersonExample
+{
+    using static System.Console;
+    using System.IO;
+
+    using ProtoBuf;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var person1 = new PersonV1.Person
+            {
+                Id = 123,
+                Name = "John",
+                Email = "john@protonmail.com",
+                Address = "Matrix",
+            };
+
+            person1.Phones.Add(
+                new PersonV1.Person.PhoneNumber
+                {
+                    Number = "010-123456",
+                    Type = PersonV1.Person.PhoneType.Home
+                }
+                );
+            person1.Phones.Add(
+                new PersonV1.Person.PhoneNumber
+                {
+                    Number = "010-012345",
+                    Type = PersonV1.Person.PhoneType.Work
+                }
+                );
+            person1.Phones.Add(
+                new PersonV1.Person.PhoneNumber
+                {
+                    Number = "123456789",
+                    Type = PersonV1.Person.PhoneType.Mobile
+                }
+                );
+
+            using (var file = File.Create("person.bin"))
+                Serializer.Serialize(file, person1);
+
+            PersonV2.Person person2;
+            using (var file = File.OpenRead("person.bin"))
+                person2 = Serializer.Deserialize<PersonV2.Person>(file);
+
+            WriteLine("Name: {0}", person2.Name);
+            WriteLine("Id: {0}", person2.Id);
+            WriteLine("Email: {0}", person2.Email);
+
+            foreach (var phone in person2.Phones)
+                WriteLine("Phone: Type: {0}, Number: {1}", phone.Type, phone.Number);
+
+            ReadKey();
+        }
+    }
+}
+
+
+// References:
+// https://github.com/protobuf-net/protobuf-net
+// http://www.ben-morris.com/protocol-buffers-protobuf-net-vs-protobuf-csharp-port/
+// https://stackoverflow.com/questions/38654856/how-to-install-protobuf-net-on-visual-studio-2012-using-nuget
