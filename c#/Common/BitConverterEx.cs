25356c3fa32b3bd94dc6c7279392d9f123aa219c
myd7349 <myd7349@gmail.com>
2019-08-21 20:36:21 +0800

    Add BitConverterEx, FileStreamExtension

---
+++ c#/Common/BitConverterEx.cs
@@ -0,0 +1,125 @@
+﻿namespace Common
+{
+    using System;
+
+    public static class BitConverterEx
+    {
+        public static byte[] GetBytes(short value, bool tolittleEndian)
+        {
+            var bytes = BitConverter.GetBytes(value);
+            if (!HasSameEndianness(tolittleEndian))
+                Array.Reverse(bytes);
+            return bytes;
+        }
+     
+        public static byte[] GetBytes(int value, bool tolittleEndian)
+        {
+            var bytes = BitConverter.GetBytes(value);
+            if (!HasSameEndianness(tolittleEndian))
+                Array.Reverse(bytes);
+            return bytes;
+        }
+        
+        public static byte[] GetBytes(long value, bool tolittleEndian)
+        {
+            var bytes = BitConverter.GetBytes(value);
+            if (!HasSameEndianness(tolittleEndian))
+                Array.Reverse(bytes);
+            return bytes;
+        }
+        
+        public static byte[] GetBytes(ushort value, bool tolittleEndian)
+        {
+            var bytes = BitConverter.GetBytes(value);
+            if (!HasSameEndianness(tolittleEndian))
+                Array.Reverse(bytes);
+            return bytes;
+        }
+        
+        public static byte[] GetBytes(uint value, bool tolittleEndian)
+        {
+            var bytes = BitConverter.GetBytes(value);
+            if (!HasSameEndianness(tolittleEndian))
+                Array.Reverse(bytes);
+            return bytes;
+        }
+        
+        public static byte[] GetBytes(ulong value, bool tolittleEndian)
+        {
+            var bytes = BitConverter.GetBytes(value);
+            if (!HasSameEndianness(tolittleEndian))
+                Array.Reverse(bytes);
+            return bytes;
+        }
+        
+        public static byte[] GetBytes(float value, bool tolittleEndian)
+        {
+            var bytes = BitConverter.GetBytes(value);
+            if (!HasSameEndianness(tolittleEndian))
+                Array.Reverse(bytes);
+            return bytes;
+        }
+        
+        public static byte[] GetBytes(double value, bool tolittleEndian)
+        {
+            var bytes = BitConverter.GetBytes(value);
+            if (!HasSameEndianness(tolittleEndian))
+                Array.Reverse(bytes);
+            return bytes;
+        }
+        
+        public static short ToInt16(byte[] bytes, int startIndex, bool isLittleEndian)
+        {
+            var value = BitConverter.ToInt16(bytes, startIndex);
+            if (!HasSameEndianness(isLittleEndian))
+                value = ByteSwap.SwapBytes(value);
+            return value;
+        }
+
+        public static int ToInt32(byte[] bytes, int startIndex, bool isLittleEndian)
+        {
+            var value = BitConverter.ToInt32(bytes, startIndex);
+            if (!HasSameEndianness(isLittleEndian))
+                value = ByteSwap.SwapBytes(value);
+            return value;
+        }
+        
+        public static long ToInt64(byte[] bytes, int startIndex, bool isLittleEndian)
+        {
+            var value = BitConverter.ToInt64(bytes, startIndex);
+            if (!HasSameEndianness(isLittleEndian))
+                value = ByteSwap.SwapBytes(value);
+            return value;
+        }
+        
+        public static ushort ToUInt16(byte[] bytes, int startIndex, bool isLittleEndian) => unchecked((ushort)ToInt16(bytes, startIndex, isLittleEndian));
+        
+        public static uint ToUInt32(byte[] bytes, int startIndex, bool isLittleEndian) => unchecked((uint)ToInt32(bytes, startIndex, isLittleEndian));
+        
+        public static ulong ToUInt64(byte[] bytes, int startIndex, bool isLittleEndian) => unchecked((ulong)ToInt64(bytes, startIndex, isLittleEndian));
+        
+        public static float ToSingle(byte[] bytes, int startIndex, bool isLittleEndian) => Int32BitsToSingle(ToInt32(bytes, startIndex, isLittleEndian));
+        
+        public static double ToDouble(byte[] bytes, int startIndex, bool isLittleEndian) => BitConverter.Int64BitsToDouble(ToInt64(bytes, startIndex, isLittleEndian));
+
+        public static unsafe int SingleToInt32Bits(float value)
+        {
+            return *((int*)&value);
+        }
+
+        public static unsafe float Int32BitsToSingle(int value)
+        {
+            return *((float*)&value);
+        }
+
+        private static bool HasSameEndianness(bool isLittleEndian)
+        {
+            return (BitConverter.IsLittleEndian && isLittleEndian)
+                || (!BitConverter.IsLittleEndian && !isLittleEndian);
+        }
+    }
+}
+
+
+// References:
+// https://github.com/dotnet/coreclr/blob/master/src/System.Private.CoreLib/shared/System/BitConverter.cs
---
+++ c#/Common/FileStreamExtension.cs
@@ -0,0 +1,63 @@
+﻿namespace Common.IO
+{
+    using System;
+    using System.IO;
+    using System.Runtime.InteropServices;
+
+    public static class FileStreamExtension
+    {
+        public static T Read<T>(this FileStream stream, Func<byte[], T> converter) where T : struct
+        {
+            if (buffer_ == null || buffer_.Length < Marshal.SizeOf(typeof(T)))
+                buffer_ = new byte[Marshal.SizeOf(typeof(T))];
+
+            if (stream.Read(buffer_, 0, Marshal.SizeOf(typeof(T))) != Marshal.SizeOf(typeof(T)))
+                throw new EndOfStreamException("No enough bytes");
+
+            return converter(buffer_);
+        }
+
+        public static short ReadInt16(this FileStream stream, bool isLittleEndian)
+        {
+            return stream.Read(bytes => BitConverterEx.ToInt16(bytes, 0, isLittleEndian));
+        }
+
+        public static ushort ReadUInt16(this FileStream stream, bool isLittleEndian)
+        {
+            return stream.Read(bytes => BitConverterEx.ToUInt16(bytes, 0, isLittleEndian));
+        }
+
+        public static int ReadInt32(this FileStream stream, bool isLittleEndian)
+        {
+            return stream.Read(bytes => BitConverterEx.ToInt32(bytes, 0, isLittleEndian));
+        }
+
+        public static uint ReadUInt32(this FileStream stream, bool isLittleEndian)
+        {
+            return stream.Read(bytes => BitConverterEx.ToUInt32(bytes, 0, isLittleEndian));
+        }
+
+        public static long ReadInt64(this FileStream stream, bool isLittleEndian)
+        {
+            return stream.Read(bytes => BitConverterEx.ToInt64(bytes, 0, isLittleEndian));
+        }
+
+        public static ulong ReadUInt64(this FileStream stream, bool isLittleEndian)
+        {
+            return stream.Read(bytes => BitConverterEx.ToUInt64(bytes, 0, isLittleEndian));
+        }
+
+        public static unsafe float ReadSingle(this FileStream stream, bool isLittleEndian)
+        {
+            var value = stream.Read(bytes => BitConverterEx.ToInt32(bytes, 0, isLittleEndian));
+            return *(float*)&value;
+        }
+
+        public static double ReadDouble(this FileStream stream, bool isLittleEndian)
+        {
+            return BitConverter.Int64BitsToDouble(stream.Read(bytes => BitConverterEx.ToInt64(bytes, 0, isLittleEndian)));
+        }
+
+        private static byte[] buffer_;
+    }
+}
