222262786e30f4675b37bd7d482fbaa519b6c002
myd7349 <myd7349@gmail.com>
2019-09-11 20:59:35 +0800

    Add marl examples and tests

---
+++ cpp/CMake/vcpkg/marl_examples/build-x86-windows.bat
@@ -0,0 +1,8 @@
+vcpkg install marl:x86-windows
+mkdir build-x86-windows
+cd build-x86-windows
+cmake .. -DCMAKE_TOOLCHAIN_FILE="%VCPKG_ROOT%\scripts\buildsystems\vcpkg.cmake" -DVCPKG_TARGET_TRIPLET=x86-windows -DCMAKE_GENERATOR_PLATFORM=Win32 -Dgtest_force_shared_crt=ON
+cmake --build . --config Release
+ctest -C Release
+cd ..
+pause
