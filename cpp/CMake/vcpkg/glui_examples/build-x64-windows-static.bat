f65cfbba2ab7e2b75bc5c92e59ad1c17db59522f
myd7349 <myd7349@gmail.com>
2019-11-30 16:43:45 +0800

    Add glui_examples

---
+++ cpp/CMake/vcpkg/glui_examples/build-x64-windows-static.bat
@@ -0,0 +1,7 @@
+vcpkg install glui:x64-windows-static
+mkdir build-x64-windows-static
+cd build-x64-windows-static
+cmake .. -DCMAKE_TOOLCHAIN_FILE="%VCPKG_ROOT%\scripts\buildsystems\vcpkg.cmake" -DVCPKG_TARGET_TRIPLET=x64-windows-static -DCMAKE_GENERATOR_PLATFORM=x64 -DUSING_MSVC_STATIC_RUNTIME=ON
+cmake --build . --config Release
+cd ..
+pause
