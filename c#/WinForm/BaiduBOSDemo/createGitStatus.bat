c974868451a1e5464772ddad514cf9a87a9ca3f2
myd7349 <myd7349@gmail.com>
2020-03-23 20:52:19 +0800

    Embed git hash into assembly info

---
+++ c#/WinForm/BaiduBOSDemo/createGitStatus.bat
@@ -0,0 +1,8 @@
+:: https://github.com/schoetbi/XPTable/blob/master/createGitStatus.bat
+git log --pretty=oneline -n1 > %1
+git status >> %1
+
+git rev-parse --short HEAD > githash.txt
+set /p HASH=<githash.txt
+echo using System.Reflection; > %2
+echo [assembly: AssemblyInformationalVersion("git:%HASH%")] >> %2
