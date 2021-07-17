a10259e0589d3edb4f3af2763abd61019fe5af19
myd7349 <myd7349@gmail.com>
2021-07-17 16:44:20 +0800

    Update

--- c#/Common/ForEach.cs
+++ c#/Common/ForEach.cs
@@ -1,5 +1,6 @@
 // 2017-07-13T10:32+08:00
 // LINQ-style ForEach to consume an enumerable object.
+// List<T>.ForEach(Action<T> action);
 using System;
 using System.Collections.Generic;
 
@@ -33,6 +34,7 @@ namespace Common.Linq
 }
 
 // References:
+// List<T>.ForEach(Action<T> action);
 // [LINQ equivalent of foreach for IEnumerable<T>](https://stackoverflow.com/questions/200574/linq-equivalent-of-foreach-for-ienumerablet)
 // [Linq Style "For Each"](https://stackoverflow.com/questions/1509442/linq-style-for-each)
 // GitHub: dotnet/corefx/src/System.Linq/src/System/Linq/Where.cs
