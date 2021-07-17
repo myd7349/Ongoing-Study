de5fb267806329f9b89d0bef29d5df90dd714942
myd7349 <myd7349@gmail.com>
2021-07-17 15:33:11 +0800

    Update

---
+++ c#/WinForm/Metafile/MetafileViewer/MetafileViewer/MetafileViewerForm.Designer.cs
@@ -0,0 +1,80 @@
+﻿
+namespace MetafileViewer
+{
+    partial class MetafileViewerForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.openButton_ = new System.Windows.Forms.Button();
+            this.saveAsButton_ = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // openButton_
+            // 
+            this.openButton_.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.openButton_.Location = new System.Drawing.Point(681, 12);
+            this.openButton_.Name = "openButton_";
+            this.openButton_.Size = new System.Drawing.Size(107, 31);
+            this.openButton_.TabIndex = 0;
+            this.openButton_.Text = "Open...";
+            this.openButton_.UseVisualStyleBackColor = true;
+            this.openButton_.Click += new System.EventHandler(this.openButton__Click);
+            // 
+            // saveAsButton_
+            // 
+            this.saveAsButton_.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.saveAsButton_.Location = new System.Drawing.Point(681, 62);
+            this.saveAsButton_.Name = "saveAsButton_";
+            this.saveAsButton_.Size = new System.Drawing.Size(107, 31);
+            this.saveAsButton_.TabIndex = 1;
+            this.saveAsButton_.Text = "Save As...";
+            this.saveAsButton_.UseVisualStyleBackColor = true;
+            // 
+            // MetafileViewerForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.saveAsButton_);
+            this.Controls.Add(this.openButton_);
+            this.Name = "MetafileViewerForm";
+            this.Text = "Metafile Viewer";
+            this.Load += new System.EventHandler(this.MetafileViewerForm_Load);
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.MetafileViewerForm_Paint);
+            this.Layout += new System.Windows.Forms.LayoutEventHandler(this.MetafileViewerForm_Layout);
+            this.Resize += new System.EventHandler(this.MetafileViewerForm_Resize);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button openButton_;
+        private System.Windows.Forms.Button saveAsButton_;
+    }
+}
+
---
+++ c#/WinForm/Metafile/MetafileViewer/MetafileViewer/MetafileViewerForm.cs
@@ -0,0 +1,102 @@
+﻿namespace MetafileViewer
+{
+    using System;
+    using System.Drawing;
+    using System.Drawing.Imaging;
+    using System.Runtime.InteropServices;
+    using System.Threading.Tasks;
+    using System.Windows.Forms;
+
+    public partial class MetafileViewerForm : Form
+    {
+        public MetafileViewerForm()
+        {
+            BackColor = Color.White;
+
+            InitializeComponent();
+        }
+
+        private void MetafileViewerForm_Load(object sender, System.EventArgs e)
+        {
+
+        }
+
+        private void MetafileViewerForm_Layout(object sender, LayoutEventArgs e)
+        {
+
+        }
+
+        private void MetafileViewerForm_Resize(object sender, System.EventArgs e)
+        {
+
+        }
+
+        private void MetafileViewerForm_Paint(object sender, PaintEventArgs e)
+        {
+            Render(e.Graphics);
+        }
+
+        private void openButton__Click(object sender, System.EventArgs e)
+        {
+            using (var openFileDialog = new OpenFileDialog())
+            {
+                var filters = new string[]
+                {
+                    "Windows Meta Files (*.emf;*.wmf)|*.emf;*.wmf",
+                    "All Files (*.*)|*.*",
+                };
+
+                openFileDialog.Filter = string.Join("|", filters);
+
+                if (openFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    if (metafile_ != null)
+                        metafile_.Dispose();
+
+                    metafile_ = new Metafile(openFileDialog.FileName);
+                    Invalidate();
+                }
+            }
+        }
+    }
+
+    public partial class MetafileViewerForm : Form
+    {
+        private void Render(Graphics graphics)
+        {
+            if (metafile_ != null)
+            {
+                //graphics.TranslateTransform(metafile_.Height, 0F);
+                graphics.RotateTransform(45F);
+                graphics.EnumerateMetafile(metafile_, Point.Empty, MetafileCallback);
+            }
+        }
+
+        private bool MetafileCallback(EmfPlusRecordType recordType, int flags,
+            int dataSize, IntPtr data, PlayRecordCallback callbackData)
+        {
+            byte[] dataArray = null;
+            if (data != IntPtr.Zero)
+            {
+                // Copy the unmanaged record to a managed byte buffer 
+                // that can be used by PlayRecord.
+                dataArray = new byte[dataSize];
+                Marshal.Copy(data, dataArray, 0, dataSize);
+            }
+
+            metafile_.PlayRecord(recordType, flags, dataSize, dataArray);
+
+            return true;
+        }
+
+        private Metafile metafile_;
+    }
+}
+
+
+// References:
+// https://docs.microsoft.com/en-us/dotnet/api/system.drawing.imaging.metafile?view=net-5.0
+// [Asynchronous programming with async and await](https://docs.microsoft.com/en-us/dotnet/csharp/programming-guide/concepts/async/)
+// [How to synchronise an asynchronous operation?](https://stackoverflow.com/questions/21173429/how-to-synchronise-an-asynchronous-operation)
+// [Non-Generic TaskCompletionSource or alternative](https://stackoverflow.com/questions/11969208/non-generic-taskcompletionsource-or-alternative)
+// [C# - asynchronous drawing on a panel](https://stackoverflow.com/questions/53570336/c-sharp-asynchronous-drawing-on-a-panel)
